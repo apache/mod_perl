7b032a6635733a15e0a9698452ea74ce21d7457d
Doug MacEachern <dougm@apache.org>
1998-04-20 09:06:13 +0000

    added PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/branches/1.x-import@176450 13f79535-47bb-0310-9956-ffa450edef68

---
+++ trunk/lib/Apache/ExtUtils.pm
@@ -0,0 +1,93 @@
+package Apache::ExtUtils;
+
+use strict;
+
+sub xs_cmd_table {
+    my($self, $class, $cmds) = @_;
+    (my $xsname = $class) =~ s/::/__/g;
+    my $modname = "XS_$xsname";
+    my $cmdtab = "";
+
+    for my $cmd (@$cmds) {
+	$cmdtab .= <<EOF;
+
+    { "$cmd", perl_cmd_perl_TAKE123,
+      (void*)"${class}::$cmd",
+      OR_ALL, TAKE123, "1-3 value(s) for $cmd" },
+EOF
+    }
+
+    return <<EOF;
+#include "modules/perl/mod_perl.h"
+
+static command_rec mod_cmds[] = {
+    $cmdtab
+    { NULL }
+};
+
+module MODULE_VAR_EXPORT $modname = {
+    STANDARD_MODULE_STUFF,
+    NULL,               /* module initializer */
+    NULL,  /* per-directory config creator */
+    NULL,   /* dir config merger */
+    NULL,       /* server config creator */
+    NULL,        /* server config merger */
+    mod_cmds,               /* command table */
+    NULL,           /* [7] list of handlers */
+    NULL,  /* [2] filename-to-URI translation */
+    NULL,      /* [5] check/validate user_id */
+    NULL,       /* [6] check user_id is valid *here* */
+    NULL,     /* [4] check access by host address */
+    NULL,       /* [7] MIME type checker/setter */
+    NULL,        /* [8] fixups */
+    NULL,             /* [10] logger */
+    NULL,      /* [3] header parser */
+    NULL,         /* process initializer */
+    NULL,         /* process exit/cleanup */
+    NULL,   /* [1] post read_request handling */
+};
+
+MODULE = $modname		PACKAGE = $modname
+
+BOOT:
+    add_module(&$modname);
+
+EOF
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Apache::ExtUtils - Utils for Apache:C/Perl glue
+
+=head1 SYNOPSIS
+
+    use Apache::ExtUtils ();
+
+=head1 DESCRIPTION
+
+Just one method at the moment:
+
+  use IO::File ();
+  use Apache::ExtUtils ();
+
+  my @directives = qw(MyDirective);
+  my $fh = IO::File->new(">My/Module/Module.xs") or die $!
+  
+  print $fh Apache::ExtUtils->xs_cmd_table("My::Module", \@directives);
+
+This example will generate a .xs file which declares an Apache C module
+for the I<My::Module> class.  It is used simply to allow Perl modules to
+add their own directives to Apache, rather than use B<PerlSetVar>.
+When the directive is encountered in a config file, a Perl subroutine of
+the same name in the I<My::Module> class is called and passed the arguments
+as perl C<TAKE123>.
+
+=head1 AUTHOR
+
+Doug MacEachern
+
+
