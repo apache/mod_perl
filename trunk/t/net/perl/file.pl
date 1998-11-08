57d5cdc6bea59f560f7375a94c7cb51c9bb4ccbb
Doug MacEachern <dougm@apache.org>
1998-11-08 01:15:20 +0000

    turns out the C stdio tmpfile() is bloody slow, if folks want it, they can use IO::File PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/branches/1.x-import@176898 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/t/net/perl/file.pl
+++ trunk/t/net/perl/file.pl
@@ -10,16 +10,16 @@ unless(have_module "Apache::File") {
 }
 
 require Apache::File;
-print "1..7\n";
+print "1..5\n";
 my $fh = Apache::File->new;
 test ++$i, $fh;
 test ++$i, $fh->open($0);
 test ++$i, !$fh->open("$0.nochance");
 test ++$i, !Apache::File->new("$0.yeahright");
-my $tmp = Apache::File->tmp;
-test ++$i, $tmp;
-++$i;
-print $tmp "ok $i\n";
-seek $tmp, 0, 0;
-print scalar(<$tmp>);
+#my $tmp = Apache::File->tmp;
+#test ++$i, $tmp;
+#++$i;
+#print $tmp "ok $i\n";
+#seek $tmp, 0, 0;
+#print scalar(<$tmp>);
 test ++$i, Apache::File->tmpfile;
