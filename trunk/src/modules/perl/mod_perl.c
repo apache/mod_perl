e497191f15d7c252659fb6e5e9d6de1673b45977
Doug MacEachern <dougm@apache.org>
1999-05-18 22:08:00 +0000

    fix $Apache::Server::Starting under dso PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/branches/1.x-import@177101 13f79535-47bb-0310-9956-ffa450edef68

--- trunk/src/modules/perl/mod_perl.c
+++ trunk/src/modules/perl/mod_perl.c
@@ -838,6 +838,7 @@ void PERL_CHILD_INIT_HOOK(server_rec *s, pool *p)
     register_cleanup(p, args, perl_child_exit_cleanup, null_cleanup);
 
     mod_perl_init_ids();
+    Apache__ServerStarting(FALSE);
     PERL_CALLBACK(hook, cls->PerlChildInitHandler);
 }
 #endif
