8a70e6bf52a0387efa9d4c0c3474ab1d62ac7f90
Doug MacEachern <dougm@apache.org>
2000-04-14 23:52:55 +0000

    change from ap_context_t to ap_pool_t integrate/test interpreter pool PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@67936 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/perl/mod_perl.c
+++ src/modules/perl/mod_perl.c
@@ -1,32 +1,86 @@
 #include "mod_perl.h"
 
-void modperl_pre_config_handler(ap_context_t *p, ap_context_t *plog,
-                                ap_context_t *ptemp)
+void modperl_startup(server_rec *s, ap_pool_t *p)
+{
+    PerlInterpreter *perl;
+    int status;
+    char *argv[] = { "httpd", "/dev/null" };
+    int argc = 2;
+
+    if (!(perl = perl_alloc())) {
+        perror("perl_alloc");
+        exit(1);
+    }
+
+    perl_construct(perl);
+    
+    status = perl_parse(perl, NULL, argc, argv, NULL);
+
+    if (status) {
+        perror("perl_parse");
+        exit(1);
+    }
+
+    perl_run(perl);
+
+    modperl_interp_pool_init(s, p, perl);
+}
+
+void modperl_init(ap_pool_t *pconf, ap_pool_t *plog, 
+                  ap_pool_t *ptemp, server_rec *s)
+{
+    modperl_startup(s, pconf);
+}
+
+void modperl_pre_config_handler(ap_pool_t *p, ap_pool_t *plog,
+                                ap_pool_t *ptemp)
 {
 }
 
-void *modperl_create_dir_config(ap_context_t *p, char *dir)
+void *modperl_create_dir_config(ap_pool_t *p, char *dir)
 {
     return NULL;
 }
 
-void *modperl_merge_dir_config(ap_context_t *p, void *base, void *add)
+void *modperl_merge_dir_config(ap_pool_t *p, void *base, void *add)
 {
     return NULL;
 }
 
-void *modperl_create_srv_config(ap_context_t *p, server_rec *s)
+modperl_srv_config_t *modperl_srv_config_new(ap_pool_t *p)
 {
-    return NULL;
+    return (modperl_srv_config_t *)
+        ap_pcalloc(p, sizeof(modperl_srv_config_t));
 }
 
-void *modperl_merge_srv_config(ap_context_t *p, void *base, void *add)
+void *modperl_create_srv_config(ap_pool_t *p, server_rec *s)
 {
-    return NULL;
+    modperl_srv_config_t *scfg = modperl_srv_config_new(p);
+
+    return scfg;
+}
+
+void *modperl_merge_srv_config(ap_pool_t *p, void *basev, void *addv)
+{
+    modperl_srv_config_t
+        *base = (modperl_srv_config_t *)basev,
+        *add  = (modperl_srv_config_t *)addv,
+        *mrg  = modperl_srv_config_new(p);
+
+    mrg->mip = add->mip ? add->mip : base->mip;
+
+    return mrg;
 }
 
 void modperl_register_hooks(void)
 {
+    /* XXX: should be pre_config hook or 1.xx logic */
+    ap_hook_open_logs(modperl_init, NULL, NULL, HOOK_MIDDLE);
+
+    /* XXX: should only bother selecting an interpreter
+     * if one is needed for the request
+     */
+    ap_hook_post_read_request(modperl_interp_select, NULL, NULL, HOOK_FIRST);
 }
 
 static command_rec modperl_cmds[] = {  
--- src/modules/perl/modperl_callback.h
+++ src/modules/perl/modperl_callback.h
@@ -1,11 +1,16 @@
-void modperl_process_callback(int idx, ap_context_t *p, server_rec *s);
+#ifndef MODPERL_CALLBACK_H
+#define MODPERL_CALLBACK_H
+
+void modperl_process_callback(int idx, ap_pool_t *p, server_rec *s);
 
 void modperl_files_callback(int idx,
-                            ap_context_t *pconf, ap_context_t *plog,
-                            ap_context_t *ptemp, server_rec *s);
+                            ap_pool_t *pconf, ap_pool_t *plog,
+                            ap_pool_t *ptemp, server_rec *s);
 
 int modperl_per_dir_callback(int idx, request_rec *r);
 
 int modperl_per_srv_callback(int idx, request_rec *r);
 
 int modperl_connection_callback(int idx, conn_rec *c);
+
+#endif /* MODPERL_CALLBACK_H */
