8b7f7fff1181aca71465c8345df25ee2e6ba48d3
Doug MacEachern <dougm@apache.org>
2000-04-17 07:10:35 +0000

    functions for tracing memory usage
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@67956 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/modules/perl/modperl_gtop.h
@@ -0,0 +1,51 @@
+#ifndef MODPERL_GTOP_H
+#define MODPERL_GTOP_H
+
+#ifdef MP_USE_GTOP
+
+#include <glibtop.h>
+#include <glibtop/open.h>
+#include <glibtop/close.h>
+#include <glibtop/xmalloc.h>
+#include <glibtop/parameter.h>
+#include <glibtop/union.h>
+#include <glibtop/sysdeps.h>
+
+#define MP_GTOP_SSS 16
+
+typedef struct {
+    char size[MP_GTOP_SSS];
+    char vsize[MP_GTOP_SSS];
+    char resident[MP_GTOP_SSS];
+    char share[MP_GTOP_SSS];
+    char rss[MP_GTOP_SSS];
+} modperl_gtop_proc_mem_ss;
+    
+typedef struct {
+    glibtop_union before;
+    glibtop_union after;
+    pid_t pid;
+    modperl_gtop_proc_mem_ss proc_mem_ss;
+} modperl_gtop_t;
+
+modperl_gtop_t *modperl_gtop_new(ap_pool_t *p);
+void modperl_gtop_get_proc_mem_before(modperl_gtop_t *gtop);
+void modperl_gtop_get_proc_mem_after(modperl_gtop_t *gtop);
+void modperl_gtop_report_proc_mem(modperl_gtop_t *gtop, 
+                                  char *when, char *msg);
+void modperl_gtop_report_proc_mem_diff(modperl_gtop_t *gtop, char *msg);
+void modperl_gtop_report_proc_mem_before(modperl_gtop_t *gtop, char *msg);
+void modperl_gtop_report_proc_mem_after(modperl_gtop_t *gtop, char *msg);
+
+#define modperl_gtop_do_proc_mem_before(msg) \
+        modperl_gtop_get_proc_mem_before(scfg->gtop); \
+        modperl_gtop_report_proc_mem_before(scfg->gtop, msg)
+
+#define modperl_gtop_do_proc_mem_after(msg) \
+        modperl_gtop_get_proc_mem_after(scfg->gtop); \
+        modperl_gtop_report_proc_mem_after(scfg->gtop, msg); \
+        modperl_gtop_report_proc_mem_diff(scfg->gtop, msg)
+
+#endif /* MP_USE_GTOP */
+
+#endif /* MODPERL_GTOP_H */
