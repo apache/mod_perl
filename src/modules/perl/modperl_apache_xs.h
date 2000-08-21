44606fbf707e7e0916b42eba996f5ab4bd36e01a
Doug MacEachern <dougm@apache.org>
2000-08-21 03:46:00 +0000

    use proper macro
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68034 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/perl/modperl_apache_xs.h
+++ src/modules/perl/modperl_apache_xs.h
@@ -1,6 +1,6 @@
 #ifndef MODPERL_APACHE_XS_H
 #define MODPERL_APACHE_XS_H
 
-apr_inline apr_size_t modperl_apache_xs_write(pTHX_ SV **mark_ptr, SV **sp_ptr);
+MP_INLINE apr_size_t modperl_apache_xs_write(pTHX_ SV **mark_ptr, SV **sp_ptr);
 
 #endif /* MODPERL_APACHE_XS_H */
