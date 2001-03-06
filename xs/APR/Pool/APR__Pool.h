1455b51c8ab94eb64e1803a92237e195ab4ef65d
Doug MacEachern <dougm@apache.org>
2001-03-06 05:16:25 +0000

    shutup fussy compiler
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68123 13f79535-47bb-0310-9956-ffa450edef68

--- xs/APR/Pool/APR__Pool.h
+++ xs/APR/Pool/APR__Pool.h
@@ -2,7 +2,7 @@
 
 static MP_INLINE apr_pool_t *mpxs_apr_pool_create(pTHX_ SV *obj)
 {
-    apr_pool_t *parent = (apr_pool_t *)mpxs_sv_object_deref(obj);
+    apr_pool_t *parent = mpxs_sv_object_deref(obj, apr_pool_t);
     apr_pool_t *retval = NULL;
     (void)apr_pool_create(&retval, parent);
     return retval;
