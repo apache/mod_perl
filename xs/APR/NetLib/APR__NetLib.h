93bc265d7ac75554cac57bf0b05a0deeddf5a438
Doug MacEachern <dougm@apache.org>
2001-03-16 00:02:50 +0000

    wrapper for apr_ipsubnet_create()
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68165 13f79535-47bb-0310-9956-ffa450edef68

---
+++ xs/APR/NetLib/APR__NetLib.h
@@ -0,0 +1,10 @@
+static MP_INLINE
+apr_ipsubnet_t *mpxs_apr_ipsubnet_create(pTHX_ SV *classname, apr_pool_t *p,
+                                         const char *ipstr,
+                                         const char *mask_or_numbits)
+{
+    apr_status_t status;
+    apr_ipsubnet_t *ipsub = NULL;
+    status = apr_ipsubnet_create(&ipsub, ipstr, mask_or_numbits, p);
+    return ipsub;
+}
