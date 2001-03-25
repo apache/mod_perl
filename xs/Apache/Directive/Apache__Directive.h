fd87fc828b1ed2a2378bb2dab5e45f018e246426
Doug MacEachern <dougm@apache.org>
2001-03-25 22:32:11 +0000

    enabled Apache::{Directive,Command,Module} modules add wrappers to access ap_conftree, "command_rec->next" and top_module PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68188 13f79535-47bb-0310-9956-ffa450edef68

---
+++ xs/Apache/Directive/Apache__Directive.h
@@ -0,0 +1,2 @@
+#define mpxs_Apache__Directive_conftree(CLASS) \
+(CLASS ? ap_conftree : ap_conftree)
---
+++ xs/Apache/Module/Apache__Module.h
@@ -0,0 +1,2 @@
+#define mpxs_Apache__Module_top_module(CLASS) \
+(CLASS ? top_module : top_module)
