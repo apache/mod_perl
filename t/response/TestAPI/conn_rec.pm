51125c4278ce557b911127c0dedd5e2ef8b0ed73
Doug MacEachern <dougm@apache.org>
2001-04-02 09:05:01 +0000

    start of apache api tests
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68208 13f79535-47bb-0310-9956-ffa450edef68

---
+++ t/response/TestAPI/conn_rec.pm
@@ -0,0 +1,55 @@
+package TestAPI::conn_rec;
+
+use Apache::Test;
+
+sub handler {
+    my $r = shift;
+
+    my $c = $r->connection;
+
+    plan $r, tests => 17;
+
+    ok $c;
+
+    ok $c->pool->isa('APR::Pool');
+
+    ok $c->base_server->isa('Apache::Server');
+
+    ok $c->client_socket->isa('APR::Socket');
+
+    ok $c->local_addr->isa('APR::SockAddr');
+
+    ok $c->remote_addr->isa('APR::SockAddr');
+
+    ok $c->remote_ip;
+
+    ok $c->remote_host || 1;
+
+    ok $c->remote_logname || 1;
+
+    ok $c->aborted || 1;
+
+    ok $c->keepalive || 1;
+
+    ok $c->keptalive || 1;
+
+    ok $c->keepalives || 1;
+
+    ok $c->local_ip;
+
+    ok $c->local_host || 1;
+
+    ok $c->id || 1;
+
+    #conn_config
+
+    ok $r->notes;
+
+    #input_filters
+    #output_filters
+    #remain
+
+    0;
+}
+
+1;
---
+++ t/response/TestAPI/request_rec.pm
@@ -0,0 +1,100 @@
+package TestAPI::request_rec;
+
+use Apache::Test;
+
+sub handler {
+    my $r = shift;
+
+    plan $r, tests => 33;
+
+    ok $r->pool->isa('APR::Pool');
+
+    ok $r->connection->isa('Apache::Connection');
+
+    ok $r->server->isa('Apache::Server');
+
+    for (qw(next prev main)) {
+        ok (! $r->$_()) || $r->$_()->isa('Apache::RequestRec');
+    }
+
+    ok $r->the_request || 1;
+
+    ok $r->assbackwards || 1;
+
+    ok $r->proxyreq || 1;
+
+    ok $r->header_only || 1;
+
+    ok $r->protocol =~ /http/i;
+
+    ok $r->proto_num;
+
+    ok $r->hostname || 1;
+
+    ok $r->request_time;
+
+    ok $r->status_line || 1;
+
+    ok $r->status || 1;
+
+    ok $r->method;
+
+    ok $r->method_number || 1;
+
+    ok $r->allowed || 1;
+
+    #allowed_xmethods
+    #allow_methods
+
+    ok $r->bytes_sent || 1;
+
+    ok $r->mtime || 1;
+
+    ok $r->headers_in;
+
+    ok $r->headers_out;
+
+    ok $r->err_headers_out;
+
+    ok $r->subprocess_env;
+
+    ok $r->notes;
+
+    ok $r->content_type;
+
+    ok $r->handler;
+
+    #content_encoding
+    #content_language
+    #content_languages
+
+    #user
+
+    #no_cache
+    #no_local_copy
+
+    ok $r->unparsed_uri;
+
+    ok $r->uri;
+
+    ok $r->filename;
+
+    ok $r->path_info || 1;
+
+    ok $r->args || 1;
+
+    #finfo
+    #parsed_uri
+
+    #per_dir_config
+    #request_config
+
+    #output_filters
+    #input_filers
+
+    #eos_sent
+
+    0;
+}
+
+1;
