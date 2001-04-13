5659c04c74cb489488a4a6f21e0c2f0eeba4a67e
Doug MacEachern <dougm@apache.org>
2001-04-13 07:17:08 +0000

    enable Apache::Filter structure interface enable Apache::FilterRec module add Apache::OutputFilter->f method for access to Apache::Filter object add filter api test PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68287 13f79535-47bb-0310-9956-ffa450edef68

---
+++ t/filter/TestFilter/api.pm
@@ -0,0 +1,74 @@
+package TestFilter::api;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Apache::Filter ();
+use Apache::FilterRec ();
+
+use Test;
+
+my $response_data = "blah blah blah";
+
+sub init_test_pm {
+    my $filter = shift;
+
+    {
+        package Apache::OutputFilter;
+        #XXX: make part of standard api?
+        unless (defined &PRINT) {
+            *PRINT = \&print;
+            *TIEHANDLE = sub { shift };
+        }
+    }
+
+    tie *STDOUT, $filter;
+
+    $Test::TESTOUT = \*STDOUT;
+    $Test::planned = 0;
+    $Test::ntest = 1;
+}
+
+sub handler {
+    my $filter = shift;
+
+    $filter->read(my $buffer); #slurp everything;
+
+    init_test_pm($filter);
+
+    plan tests => 6;
+
+    ok $buffer eq $response_data;
+
+    my $f = $filter->f;
+
+    ok $f->isa('Apache::Filter');
+
+    my $frec = $f->frec;
+
+    ok $frec->isa('Apache::FilterRec');
+
+    ok $frec->name;
+
+    my $r = $f->r;
+
+    ok $r->isa('Apache::RequestRec');
+
+    ok $r->uri eq '/' . __PACKAGE__;
+
+    0;
+}
+
+sub response {
+    my $r = shift;
+
+    $r->content_type('text/plain');
+    $r->puts($response_data);
+
+    Apache::OK;
+}
+
+1;
+__DATA__
+SetHandler modperl
+PerlResponseHandler TestFilter::api::response
