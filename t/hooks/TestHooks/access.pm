11e0f3c57973ea50a28161ea947611c1b8d0339a
Doug MacEachern <dougm@apache.org>
2001-04-13 01:35:15 +0000

    add PerlAccessHandler test
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68283 13f79535-47bb-0310-9956-ffa450edef68

---
+++ t/hooks/TestHooks/access.pm
@@ -0,0 +1,25 @@
+package TestHooks::access;
+
+use strict;
+use warnings FATAL => 'all';
+
+use APR::Table ();
+use Apache::Access ();
+use Apache::Const -compile => qw(OK FORBIDDEN);
+
+my $allowed_ips = qr{^(10|127)\.};
+
+sub handler {
+    my $r = shift;
+
+    my $fake_ip = $r->headers_in->get('X-Forwarded-For') || "";
+
+    return Apache::FORBIDDEN unless $fake_ip =~ $allowed_ips;
+
+    Apache::OK;
+}
+
+1;
+__DATA__
+PerlResponseHandler Apache::TestHandler::ok1
+SetHandler modperl
---
+++ t/hooks/access.t
@@ -0,0 +1,21 @@
+use strict;
+use warnings FATAL => 'all';
+
+use Apache::Test;
+use Apache::TestRequest;
+
+plan tests => 4, \&have_lwp;
+
+my $location = "/TestHooks::access";
+
+ok ! GET_OK $location;
+
+my $rc = GET_RC $location;
+
+ok $rc == 403;
+
+ok GET_OK $location, 'X-Forwarded-For' => '127.0.0.1';
+
+ok ! GET_OK $location, 'X-Forwarded-For' => '666.0.0.1';
+
+
