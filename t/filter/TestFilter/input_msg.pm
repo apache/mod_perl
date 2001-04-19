b2fd84710e341b2735d5430edd8d69d9dcd3eb7c
Doug MacEachern <dougm@apache.org>
2001-04-19 21:28:03 +0000

    add test for InputFilterMessage handler
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68323 13f79535-47bb-0310-9956-ffa450edef68

---
+++ t/filter/TestFilter/input_msg.pm
@@ -0,0 +1,68 @@
+package TestFilter::input_msg;
+
+use strict;
+use warnings FATAL => 'all';
+
+use base qw(Apache::Filter);
+
+use Test;
+use Apache::Test ();
+use APR::Brigade ();
+use APR::Bucket ();
+
+#XXX
+@Apache::InputFilter::ISA = qw(Apache::OutputFilter);
+
+my $from_url = '/input_filter.html';
+my $to_url = '/TestFilter::input_msg::response';
+
+sub handler : InputFilterMessage {
+    my($filter, $bb, $mode) = @_;
+
+    if ($bb->empty) {
+        my $rv = $filter->f->next->get_brigade($bb, $mode);
+
+        if ($rv != APR::SUCCESS) {
+            return $rv;
+        }
+    }
+
+    for (my $bucket = $bb->first; $bucket; $bucket = $bb->next($bucket)) {
+        my $data;
+        my $status = $bucket->read($data);
+
+        $bucket->remove;
+
+        if ($data and $data =~ s,GET $from_url,GET $to_url,) {
+            $bb->insert_tail(APR::Bucket->new($data));
+        }
+        else {
+            $bb->insert_tail($bucket);
+        }
+    }
+
+    Apache::OK;
+}
+
+sub response {
+    my $r = shift;
+
+    $r->content_type('text/plain');
+
+    $r->puts("1..1\nok 1\n");
+
+    Apache::OK;
+}
+
+1;
+__DATA__
+<VirtualHost TestFilter::input_msg>
+
+  PerlInputFilterHandler TestFilter::input_msg
+
+  <Location /TestFilter::input_msg::response>
+     SetHandler modperl
+     PerlResponseHandler TestFilter::input_msg::response
+  </Location>
+
+</VirtualHost>
