07044fa525f88c0167b719607716b90ce86335a0
Doug MacEachern <dougm@apache.org>
2001-04-19 17:36:34 +0000

    move read_post util so it can be used by other tests
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68308 13f79535-47bb-0310-9956-ffa450edef68

--- t/conf/modperl_extra.pl
+++ t/conf/modperl_extra.pl
@@ -6,4 +6,21 @@ use Apache::Connection ();
 
 use Apache::Const -compile => ':common';
 
+use APR::Table ();
+
+sub ModPerl::Test::read_post {
+    my $r = shift;
+
+    $r->setup_client_block;
+
+    return undef unless $r->should_client_block;
+
+    my $len = $r->headers_in->get('content-length');
+
+    my $buf;
+    $r->get_client_block($buf, $len);
+
+    return $buf;
+}
+
 1;
