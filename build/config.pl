11401db7c7b6da7c652978cfdeb0dec411cfc26c
Stas Bekman <stas@apache.org>
2001-04-20 18:08:06 +0000

    * moving the code from util/config.pl into a new lib/ModPerl/Config.pm * now util/config.pl uses lib/ModPerl/Config.pm * a new bugreport util uses lib/ModPerl/Config.pm
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68336 13f79535-47bb-0310-9956-ffa450edef68

--- build/config.pl
+++ build/config.pl
@@ -1,27 +1,9 @@
-use lib qw(lib);
-use lib qw(Apache-Test/lib);
+#!/usr/bin/perl -w
 
-use Apache::Build ();
-use Apache::TestConfig ();
+use strict;
+use FindBin qw($Bin);
+use lib "$Bin/../lib";
 
-my $build_config = Apache::Build->build_config;
+use ModPerl::Config ();
 
-print "using $INC{'Apache/BuildConfig.pm'}\n";
-
-print "Makefile.PL options:\n";
-for (sort keys %$build_config) {
-    next unless /^MP_/;
-    printf "    %-20s => %s\n", $_, $build_config->{$_};
-}
-
-my $test_config = Apache::TestConfig->new;
-my $httpd = $test_config->{vars}->{httpd};
-
-print "\n$httpd -V:\n";
-system "$httpd -V";
-
-my $perl = $build_config->{MODPERL_PERLPATH};
-
-print "\n$perl -V:\n";
-system "$perl -V";
-
+print ModPerl::Config::config_as_str();
---
+++ lib/ModPerl/Config.pm
@@ -0,0 +1,47 @@
+package ModPerl::Config;
+
+use lib qw(Apache-Test/lib);
+
+use Apache::Build ();
+use Apache::TestConfig ();
+
+sub config_as_str{
+    my $build_config = Apache::Build->build_config;
+
+    my $cfg = '';
+
+    $cfg .= "*** using $INC{'Apache/BuildConfig.pm'}\n";
+
+    $cfg .= "*** Makefile.PL options:\n";
+    $cfg .= join '',
+        map {sprintf "    %-20s => %s\n", $_, $build_config->{$_}}
+            grep /^MP_/, sort keys %$build_config;
+
+    my $test_config = Apache::TestConfig->new;
+    my $httpd = $test_config->{vars}->{httpd};
+    $command = "$httpd -v";
+    $cfg .= "\n\n*** $command\n";
+    $cfg .= qx{$command};
+
+    my $perl = $build_config->{MODPERL_PERLPATH};
+    $command = "$perl -V";
+    $cfg .= "\n\n*** $command\n";
+    $cfg .= qx{$command};
+
+    return $cfg;
+
+}
+
+1;
+__END__
+
+=pod
+
+=head1 NAME - ModPerl::Config
+
+=head1 DESCRIPTION
+
+Functions to retrieve mod_perl specific env information.
+
+=cut
+
