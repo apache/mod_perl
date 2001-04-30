fa48d3d44755a8af0cc34b98857a4c8ca1c41873
Doug MacEachern <dougm@apache.org>
2001-04-30 04:38:35 +0000

    integrate modperl_global module and initialize pconf/request_rec globals
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68370 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/perl/mod_perl.c
+++ src/modules/perl/mod_perl.c
@@ -207,6 +207,12 @@ static void modperl_init_clones(server_rec *s, apr_pool_t *p)
 }
 #endif /* USE_ITHREADS */
 
+static void modperl_init_globals(apr_pool_t *pconf)
+{
+    modperl_global_init_pconf(pconf, (void *)pconf);
+    modperl_tls_create_request_rec(pconf);
+}
+
 void modperl_hook_init(apr_pool_t *pconf, apr_pool_t *plog, 
                        apr_pool_t *ptemp, server_rec *s)
 {
@@ -237,6 +243,7 @@ static void modperl_hook_post_config(apr_pool_t *pconf, apr_pool_t *plog,
     ap_add_version_component(pconf,
                              Perl_form(aTHX_ "Perl/v%vd", PL_patchlevel));
     modperl_mgv_hash_handlers(pconf, s);
+    modperl_init_globals(pconf);
 #ifdef USE_ITHREADS
     modperl_init_clones(s, pconf);
 #endif
@@ -253,11 +260,17 @@ static int modperl_hook_create_request(request_rec *r)
 
 static int modperl_hook_post_read_request(request_rec *r)
 {
+    /* if 'PerlOptions +GlobalRequest' is outside a container */
+    modperl_global_request_cfg_set(r);
+
     return modperl_input_filter_register_request(r);
 }
 
 static int modperl_hook_header_parser(request_rec *r)
 {
+    /* if 'PerlOptions +GlobalRequest' is inside a container */
+    modperl_global_request_cfg_set(r);
+
     return modperl_input_filter_register_request(r);
 }
 
