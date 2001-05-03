9cc17ef783cd2577a760feefcf518db62282c87f
Doug MacEachern <dougm@apache.org>
2001-05-03 06:45:25 +0000

    untabify
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68380 13f79535-47bb-0310-9956-ffa450edef68

--- xs/Apache/Log/Apache__Log.h
+++ xs/Apache/Log/Apache__Log.h
@@ -1,9 +1,9 @@
 static void mpxs_Apache__Log_BOOT(pTHXo)
 {
     av_push(get_av("Apache::Log::Request::ISA",TRUE), 
-	    newSVpv("Apache::Log",11));
+            newSVpv("Apache::Log",11));
     av_push(get_av("Apache::Log::Server::ISA",TRUE), 
-	    newSVpv("Apache::Log",11));
+            newSVpv("Apache::Log",11));
 }
 
 #define croak_inval_obj() \
@@ -22,11 +22,11 @@ static void mpxs_ap_log_error(pTHX_ int level, SV *sv, SV *msg)
     request_rec *r = NULL;
 
     if (SvROK(sv) && sv_isa(sv, "Apache::Log::Request")) {
-	r = (request_rec *)SvObjIV(sv);
-	s = r->server;
+        r = (request_rec *)SvObjIV(sv);
+        s = r->server;
     }
     else if (SvROK(sv) && sv_isa(sv, "Apache::Log::Server")) {
-	s = (server_rec *)SvObjIV(sv);
+        s = (server_rec *)SvObjIV(sv);
     }
     else {
         croak_inval_obj();
@@ -34,33 +34,33 @@ static void mpxs_ap_log_error(pTHX_ int level, SV *sv, SV *msg)
 
     if ((lmask == APLOG_DEBUG) && (s->loglevel >= APLOG_DEBUG)) {
         COP *cop = cxstack[1].blk_oldcop;
-	file = CopFILE(cop); /* (caller)[1] */
-	line = CopLINE(cop); /* (caller)[2] */
+        file = CopFILE(cop); /* (caller)[1] */
+        line = CopLINE(cop); /* (caller)[2] */
     }
 
     if ((s->loglevel >= lmask) && 
         SvROK(msg) && (SvTYPE(SvRV(msg)) == SVt_PVCV))
     {
-	dSP;
-	ENTER;SAVETMPS;
-	PUSHMARK(sp);
-	(void)call_sv(msg, G_SCALAR);
-	SPAGAIN;
-	svstr = POPs;
-	(void)SvREFCNT_inc(svstr);
-	PUTBACK;
-	FREETMPS;LEAVE;
-	str = SvPV(svstr,n_a);
+        dSP;
+        ENTER;SAVETMPS;
+        PUSHMARK(sp);
+        (void)call_sv(msg, G_SCALAR);
+        SPAGAIN;
+        svstr = POPs;
+        (void)SvREFCNT_inc(svstr);
+        PUTBACK;
+        FREETMPS;LEAVE;
+        str = SvPV(svstr,n_a);
     }
     else {
-	str = SvPV(msg,n_a);
+        str = SvPV(msg,n_a);
     }
 
     if (r) {
-	ap_log_rerror(file, line, APLOG_NOERRNO|level, 0, r, "%s", str);
+        ap_log_rerror(file, line, APLOG_NOERRNO|level, 0, r, "%s", str);
     }
     else {
-	ap_log_error(file, line, APLOG_NOERRNO|level, 0, s, "%s", str);
+        ap_log_error(file, line, APLOG_NOERRNO|level, 0, s, "%s", str);
     }
 
     if (svstr) {
@@ -84,11 +84,11 @@ static SV *mpxs_Apache__Log_log(pTHX_ SV *sv, int logtype)
     switch (logtype) {
       case MP_LOG_REQUEST:
         pclass = "Apache::Log::Request";
-	retval = (void *)modperl_sv2request_rec(aTHX_ sv);
+        retval = (void *)modperl_sv2request_rec(aTHX_ sv);
         break;
       case MP_LOG_SERVER:
-	pclass = "Apache::Log::Server";
-	retval = (void *)SvObjIV(sv);
+        pclass = "Apache::Log::Server";
+        retval = (void *)SvObjIV(sv);
         break;
       default:
         croak_inval_obj();
