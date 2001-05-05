9bc527a91b1fca1d54a4067cd535aaff49436421
Doug MacEachern <dougm@apache.org>
2001-05-05 01:17:39 +0000

    add tests for Apache::Response
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68404 13f79535-47bb-0310-9956-ffa450edef68

---
+++ t/response/TestAPI/response.pm
@@ -0,0 +1,39 @@
+package TestAPI::response;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Apache::Response ();
+use Apache::Test;
+
+sub handler {
+    my $r = shift;
+
+    plan $r, tests => 6;
+
+    ok $r->make_etag(0);
+
+    $r->set_content_length(0);
+
+    ok 1;
+
+    ok $r->meets_conditions || 1;
+
+    ok $r->rationalize_mtime(time) >= $r->request_time;
+
+    my $mtime = (stat __FILE__)[9];
+
+    $r->update_mtime($mtime);
+
+    ok $r->mtime == $mtime;
+
+    $r->set_last_modified;
+
+    $r->custom_response(500, "xxx");
+
+    ok 1;
+
+    Apache::OK;
+}
+
+1;
