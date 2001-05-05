12e5c7bbc34623866e97b7dc7c559e353d2e2747
Doug MacEachern <dougm@apache.org>
2001-05-05 02:08:06 +0000

    add tests for Apache::Access
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68406 13f79535-47bb-0310-9956-ffa450edef68

---
+++ t/response/TestAPI/access.pm
@@ -0,0 +1,41 @@
+package TestAPI::access;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Apache::Const -compile => qw(:options :override :satisfy);
+use Apache::Access ();
+use Apache::Test;
+
+sub handler {
+    my $r = shift;
+
+    plan $r, tests => 8;
+
+    $r->allow_methods(1, qw(GET POST));
+
+    ok 1;
+
+    ok $r->allow_options & Apache::OPT_INDEXES;
+
+    ok !($r->allow_options & Apache::OPT_EXECCGI);
+
+    ok $r->allow_overrides & Apache::OR_LIMIT;
+
+    ok $r->satisfies == Apache::SATISFY_NOSPEC;
+
+    ok $r->auth_name eq 'modperl';
+
+    ok $r->auth_type eq 'none';
+
+    ok ! $r->some_auth_required;
+
+    Apache::OK;
+}
+
+1;
+__END__
+Options None
+Options Indexes FollowSymLinks
+AuthName modperl
+AuthType none
