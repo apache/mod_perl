0050e34c3b747b8197d54cf2f5417d9a1ef43eb5
Doug MacEachern <dougm@apache.org>
2001-05-08 20:15:13 +0000

    do not need to copy the buffer to be parsed
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68425 13f79535-47bb-0310-9956-ffa450edef68

--- xs/Apache/Response/Apache__Response.h
+++ xs/Apache/Response/Apache__Response.h
@@ -1,6 +1,5 @@
-/* 
- * pretty sure we need to copy SvPVX, otherwise it will be modified inline
- * XXX: that might be a desired feature
+/*
+ * XXX: should do something useful if we end up with any bodytext
  */
 /* XXX: this should probably named $r->cgi_header_parse
  * and send_cgi_header an alias in Apache::compat
@@ -8,9 +7,6 @@
 #define mpxs_Apache__RequestRec_send_cgi_header(r, sv) \
 { \
     STRLEN len; \
-    char *buff = SvPV(sv, len); \
-    char *copy = apr_palloc(r->pool, len+1); \
     const char *bodytext; \
-    apr_cpystrn(copy, buff, len+1); \
-    modperl_cgi_header_parse(r, copy, &bodytext); \
+    modperl_cgi_header_parse(r, SvPV(sv,len), &bodytext); \
 }
