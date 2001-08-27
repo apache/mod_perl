7e7a1db5f1c5b5c2750a58a2f661e987c6af980d
Doug MacEachern <dougm@apache.org>
2001-08-27 17:14:48 +0000

    adjust to bucket/sms changes
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68519 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/perl/modperl_bucket.c
+++ src/modules/perl/modperl_bucket.c
@@ -9,6 +9,7 @@ typedef struct {
     apr_bucket_refcount refcount;
     SV *sv;
     PerlInterpreter *perl;
+    apr_sms_t *sms;
 } modperl_bucket_sv_t;
 
 static apr_status_t
@@ -44,12 +45,7 @@ static void modperl_bucket_sv_destroy(void *data)
 
     SvREFCNT_dec(svbucket->sv);
 
-    safefree(svbucket);
-}
-
-static void modperl_bucket_sv_free(void *e)
-{
-    Safefree(e);
+    apr_sms_free(svbucket->sms, svbucket);
 }
 
 static const apr_bucket_type_t modperl_bucket_sv_type = {
@@ -68,7 +64,10 @@ static apr_bucket *modperl_bucket_sv_make(pTHX_
 {
     modperl_bucket_sv_t *svbucket; 
 
-    Newz(0, svbucket, 1, modperl_bucket_sv_t);
+    svbucket = (modperl_bucket_sv_t *)apr_sms_malloc(bucket->sms,
+                                                     sizeof(*svbucket));
+
+    svbucket->sms = bucket->sms;
 
     bucket = apr_bucket_shared_make(bucket, svbucket, offset, offset+len);
 
@@ -79,7 +78,7 @@ static apr_bucket *modperl_bucket_sv_make(pTHX_
     svbucket->sv = sv;
 
     if (!bucket) {
-        Safefree(svbucket);
+        apr_sms_free(svbucket->sms, svbucket);
         return NULL;
     }
 
@@ -88,8 +87,6 @@ static apr_bucket *modperl_bucket_sv_make(pTHX_
     MP_TRACE_f(MP_FUNC, "sv=0x%lx, refcnt=%d\n",
                (unsigned long)sv, SvREFCNT(sv));
 
-    bucket->free = modperl_bucket_sv_free;
-
     bucket->type = &modperl_bucket_sv_type;
 
     return bucket;
@@ -97,7 +94,17 @@ static apr_bucket *modperl_bucket_sv_make(pTHX_
 
 apr_bucket *modperl_bucket_sv_create(pTHX_ SV *sv, int offset, int len)
 {
-    apr_bucket *bucket = (apr_bucket *)safemalloc(sizeof(*bucket));
+    apr_sms_t *sms;
+    apr_bucket *bucket;
+
+    if (!apr_bucket_global_sms) {
+        apr_sms_std_create(&apr_bucket_global_sms);
+    }
+
+    sms = apr_bucket_global_sms;
+    bucket = (apr_bucket *)apr_sms_malloc(sms, sizeof(*bucket));
     APR_BUCKET_INIT(bucket);
+    bucket->sms = sms;
+
     return modperl_bucket_sv_make(aTHX_ bucket, sv, offset, len);
 }
