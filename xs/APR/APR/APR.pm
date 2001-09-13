9cc2b00afae805f4f46fbb72ba752ce5476055d8
Doug MacEachern <dougm@apache.org>
2001-09-13 02:40:33 +0000

    only bootstrap outside of mod_perl
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68549 13f79535-47bb-0310-9956-ffa450edef68

--- xs/APR/APR/APR.pm
+++ xs/APR/APR/APR.pm
@@ -7,7 +7,10 @@ our @ISA = qw(DynaLoader);
 #dlopen("APR.so", RTDL_GLOBAL); so we only need to link libapr.a once
 sub dl_load_flags { 0x01 }
 
-__PACKAGE__->bootstrap($VERSION);
+unless (defined &APR::XSLoader::BOOTSTRAP) {
+    __PACKAGE__->bootstrap($VERSION);
+    *APR::XSLoader::BOOTSTRAP = sub () { 1 };
+}
 
 1;
 __END__
