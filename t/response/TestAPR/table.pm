99e813cb475bf1ba37cd690a738c36f6a9f54dc1
Doug MacEachern <dougm@apache.org>
2001-09-15 18:17:31 +0000

    implement APR::Table->do PR: Obtained from: Submitted by:	Philippe M . Chiasson <gozer@cpan.org> Reviewed by:	dougm
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68556 13f79535-47bb-0310-9956-ffa450edef68

--- t/response/TestAPR/table.pm
+++ t/response/TestAPR/table.pm
@@ -8,10 +8,13 @@ use Apache::Test;
 use Apache::Const -compile => 'OK';
 use APR::Table ();
 
+my $filter_count;
+my $TABLE_SIZE = 20;
+
 sub handler {
     my $r = shift;
 
-    plan $r, tests => 5;
+    plan $r, tests => 9;
 
     my $table = APR::Table::make($r->pool, 16);
 
@@ -25,7 +28,57 @@ sub handler {
 
     ok not defined $table->get('foo');
 
+    for (1..$TABLE_SIZE) {
+        $table->set(chr($_+97), $_);
+    }
+
+    #Simple filtering
+    $filter_count = 0;
+    $table->do("my_filter");
+    ok $filter_count == $TABLE_SIZE;
+
+    #Filtering aborting in the middle
+    $filter_count = 0;
+    $table->do("my_filter_stop");
+    ok $filter_count == int($TABLE_SIZE)/2;
+
+    #Filtering with anon sub
+    $filter_count=0;
+    $table->do(sub {
+        my ($key,$value) = @_;
+        $filter_count++;
+        unless ($key eq chr($value+97)) {
+            die "arguments I recieved are bogus($key,$value)";
+        }
+        return 1;
+    });
+
+    ok $filter_count == $TABLE_SIZE;
+
+    $filter_count = 0;
+    $table->do("my_filter", "c", "b", "e");
+    ok $filter_count == 3;
+
     Apache::OK;
+}
+
+sub my_filter {
+    my ($key,$value) = @_;
+    $filter_count++;
+    unless ($key eq chr($value+97)) {
+        die "arguments I recieved are bogus($key,$value)";
+    }
+    return 1;
+}
+
+sub my_filter_stop {
+    my ($key,$value) = @_;
+    $filter_count++;
+    unless ($key eq chr($value+97)) {
+        die "arguments I recieved are bogus($key,$value)";
+    }
+    return 0 if ($filter_count == int($TABLE_SIZE)/2);
+    return 1;
 }
 
 1;
