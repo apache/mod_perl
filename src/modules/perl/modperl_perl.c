32c02f766dc61af914053de2ee5000cea633942f
Doug MacEachern <dougm@apache.org>
2001-09-16 00:56:15 +0000

    init ids for ithread Perls
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68564 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/perl/modperl_perl.c
+++ src/modules/perl/modperl_perl.c
@@ -4,17 +4,37 @@
  * others (larger tweaks) are in their own modules, e.g. modperl_env.c
  */
 
-void modperl_perl_init_ids(pTHX)
+void modperl_perl_ids_get(modperl_perl_ids_t *ids)
 {
-    sv_setiv(GvSV(gv_fetchpv("$", TRUE, SVt_PV)), (I32)getpid());
-
+    ids->pid  = (I32)getpid();
 #ifndef WIN32
-    PL_uid  = (int)getuid(); 
-    PL_euid = (int)geteuid(); 
-    PL_gid  = (int)getgid(); 
-    PL_egid = (int)getegid(); 
+    ids->uid  = getuid();
+    ids->euid = geteuid(); 
+    ids->gid  = getgid(); 
+    ids->gid  = getegid(); 
+
     MP_TRACE_g(MP_FUNC, 
                "uid=%d, euid=%d, gid=%d, egid=%d\n",
-               PL_uid, PL_euid, PL_gid, PL_egid);
+               (int)ids->uid, (int)ids->euid,
+               (int)ids->gid, (int)ids->egid);
 #endif
+}
+
+void modperl_perl_init_ids(pTHX_ modperl_perl_ids_t *ids)
+{
+    sv_setiv(GvSV(gv_fetchpv("$", TRUE, SVt_PV)), ids->pid);
+
+#ifndef WIN32
+    PL_uid  = ids->uid;
+    PL_euid = ids->euid;
+    PL_gid  = ids->gid;
+    PL_egid = ids->egid;
+#endif
+}
+
+apr_status_t modperl_perl_init_ids_mip(pTHX_ modperl_interp_pool_t *mip,
+                                       void *data)
+{
+    modperl_perl_init_ids(aTHX_ (modperl_perl_ids_t *)data);
+    return APR_SUCCESS;
 }
--- src/modules/perl/modperl_perl.h
+++ src/modules/perl/modperl_perl.h
@@ -1,6 +1,17 @@
 #ifndef MODPERL_PERL_H
 #define MODPERL_PERL_H
 
-void modperl_perl_init_ids(pTHX);
+typedef struct {
+    I32 pid;
+    Uid_t uid, euid;
+    Gid_t gid, egid;
+} modperl_perl_ids_t;
+
+void modperl_perl_ids_get(modperl_perl_ids_t *ids);
+
+void modperl_perl_init_ids(pTHX_ modperl_perl_ids_t *ids);
+
+apr_status_t modperl_perl_init_ids_mip(pTHX_ modperl_interp_pool_t *mip,
+                                       void *data);
 
 #endif /* MODPERL_PERL_H */
