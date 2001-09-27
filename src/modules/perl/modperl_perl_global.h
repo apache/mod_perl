bef231721ab63c44ba75e8844b536f0e160a6ecd
Doug MacEachern <dougm@apache.org>
2001-09-27 19:03:19 +0000

    new module for managing Perl globals
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68603 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/modules/perl/modperl_perl_global.h
@@ -0,0 +1,31 @@
+#ifndef MODPERL_PERL_GLOBAL_H
+#define MODPERL_PERL_GLOBAL_H
+
+typedef struct {
+    GV *gv;
+    AV *tmpav;
+    AV *origav;
+} modperl_perl_global_gvav_t;
+
+typedef struct {
+    GV *gv;
+    char flags;
+} modperl_perl_global_gvio_t;
+
+typedef struct {
+    SV *sv;
+    char pv[256]; /* XXX: only need enough for $/ at the moment */
+    I32 cur;
+} modperl_perl_global_svpv_t;
+
+typedef struct {
+    modperl_perl_global_gvav_t inc;
+    modperl_perl_global_gvio_t defout;
+    modperl_perl_global_svpv_t rs;
+} modperl_perl_globals_t;
+
+void modperl_perl_global_save(pTHX_ modperl_perl_globals_t *globals);
+
+void modperl_perl_global_restore(pTHX_ modperl_perl_globals_t *globals);
+
+#endif
