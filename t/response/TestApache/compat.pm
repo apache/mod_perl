395cfdbd1321ae1dd919f4785f3af7e6bae03a7e
Stas Bekman <stas@apache.org>
2001-09-29 19:33:39 +0000

    - prepare a ground for adding many new sub-tests for Apache::compat - add the content-type test for send_http_header($val); - add tests for $r->header_{in|out} (5,7,9,11 are failing!) PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68633 13f79535-47bb-0310-9956-ffa450edef68

--- t/response/TestApache/compat.pm
+++ t/response/TestApache/compat.pm
@@ -5,7 +5,8 @@ use warnings FATAL => 'all';
 
 use Apache::compat ();
 
-use Apache::Constants qw(OK M_POST);
+use Apache::TestUtil;
+use Apache::Constants qw(OK M_POST DECLINED);
 
 sub handler {
     my $r = shift;
@@ -20,7 +21,55 @@ sub handler {
         %data = $r->Apache::args;
     }
 
-    $r->print("ok $data{ok}");
+    return DECLINED unless exists $data{test};
+
+    if ($data{test} eq 'content' || $data{test} eq 'args') {
+        $r->print("test $data{test}");
+    }
+    elsif ($data{test} eq 'header') {
+        my $way      = $data{way};
+        my $sub      = "header_$way";
+        my $sub_good = "headers_$way";
+        if ($data{what} eq 'get_scalar') {
+            # get in scalar ctx
+            my $key;
+            if ($way eq 'in') {
+                $key = "user-agent"; # should exist with lwp
+            }
+            else {
+                # outgoing headers aren't set yet, so we set one
+                $key = "X-barabara";
+                $r->$sub_good->set($key, $key x 2);
+            }
+            my $exp = $r->$sub_good->get($key);
+            my $got = $r->$sub($key);
+            $r->print(t_is_equal($exp, $got) ? 'ok' : 'nok');
+        }
+        elsif ($data{what} eq 'get_list') {
+            # get in list ctx
+            my $key = $data{test};
+            my @exp = qw(foo bar);
+            $r->$sub_good->add($key => $_) for @exp;
+            my @got = $r->$sub($key);
+            $r->print(t_is_equal(\@exp, \@got) ? 'ok' : 'nok');
+        }
+        elsif ($data{what} eq 'set') {
+            # set
+            my $key = $data{test};
+            my $exp = $key x 2;
+            $r->$sub($key => $exp);
+            my $got = $r->$sub($key);
+            $r->print(t_is_equal($exp, $got) ? 'ok' : 'nok');
+        }
+        elsif ($data{what} eq 'unset') {
+            # unset
+            my $key = $data{test};
+            my $exp = undef;
+            $r->$sub($key => $exp);
+            my $got = $r->$sub($key);
+            $r->print(t_is_equal($exp, $got) ? 'ok' : 'nok');
+        }
+    }
 
     OK;
 }
