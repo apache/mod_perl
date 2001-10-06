92bb52e1fbc7df47b300177248395fab9365193a
Doug MacEachern <dougm@apache.org>
2001-10-06 01:03:27 +0000

    add Apache::Module::loaded function and Apache::module wrapper in Apache::compat PR: Obtained from: Submitted by:	Philippe M . Chiasson <gozer@cpan.org> Reviewed by:	dougm
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68643 13f79535-47bb-0310-9956-ffa450edef68

--- xs/Apache/Module/Apache__Module.h
+++ xs/Apache/Module/Apache__Module.h
@@ -1,2 +1,40 @@
 #define mpxs_Apache__Module_top_module(CLASS) \
 (CLASS ? ap_top_module : ap_top_module)
+
+static MP_INLINE int mpxs_Apache__Module_loaded(char *name)
+{
+    char nameptr[256];
+    char *base;
+    module *modp;
+
+    /* Does the module name have a '.' in it ? */
+    if ((base = ap_strchr(name, '.'))) {
+        int len = base - name;
+
+        memcpy(nameptr, name, len);
+        memcpy(nameptr + len, ".c\0", 3);
+
+        /* check if module is loaded */
+        if (!(modp = ap_find_linked_module(nameptr))) {
+            return 0;
+        }
+
+        if (*(base + 1) == 'c') {
+            return 1;
+        }
+
+        /* if it ends in '.so', check if it was dynamically loaded */
+        if ((strlen(base+1) == 2) &&
+            (*(base + 1) == 's') && (*(base + 2) == 'o') &&
+            modp->dynamic_load_handle)
+        {
+            return 1;
+        }
+
+        return 0;
+    }
+    else {
+        dTHX; /*XXX*/ 
+        return modperl_perl_module_loaded(aTHX_ name);
+    }
+}
