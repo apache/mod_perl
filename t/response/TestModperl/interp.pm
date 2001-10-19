2239ba34d3c9bec1e8d5648c4cecba45a0b1b594
Stas Bekman <stas@apache.org>
2001-10-19 02:09:24 +0000

    - update the 'same interpreter' test to use the abstracted functionality which has moved into the core of Apache::Test PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68726 13f79535-47bb-0310-9956-ffa450edef68

--- t/response/TestModperl/interp.pm
+++ t/response/TestModperl/interp.pm
@@ -1,53 +1,22 @@
-package TestModperl::interp;
+package TestModperl::interp1;
 
 use warnings FATAL => 'all';
 use strict;
 
-use APR::UUID ();
-use Apache::Const -compile => qw(OK NOT_FOUND SERVER_ERROR);
+use Apache::Const -compile => qw(OK);
 
-use constant INTERP => 'X-PerlInterpreter';
-
-my $interp_id = "";
-my $value = 0;
-
-sub fixup {
-    my $r = shift;
-    my $interp = $r->headers_in->get(INTERP);
-    my $rc = Apache::OK;
-
-    unless ($interp) {
-        #shouldn't be requesting this without an INTERP header
-        return Apache::SERVER_ERROR;
-    }
-
-    my $id = $interp_id;
-    if ($interp eq 'init') { #first request for an interpreter instance
-        #unique id for this instance
-        $interp_id = $id = APR::UUID->new->format;
-        $value = 0; #reset our global data
-    }
-    elsif ($interp ne $interp_id) {
-        #this is not the request interpreter instance
-        $rc = Apache::NOT_FOUND;
-    }
-
-    #so client can save the created instance id or check the existing value
-    $r->headers_out->set(INTERP, $id);
-
-    return $rc;
-}
+my $value = '';
 
 sub handler {
     my $r = shift;
 
-    #test the actual global data
-    $value++;
-    $r->puts("ok $value\n");
+    # test the actual global data
+    $value = Apache::TestHandler::same_interp_counter();
+    $r->puts($value);
 
     Apache::OK;
 }
 
 1;
 __END__
-PerlFixupHandler TestModperl::interp::fixup
+PerlFixupHandler Apache::TestHandler::same_interp_fixup
