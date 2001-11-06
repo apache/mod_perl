93d8272968731a926da6c5c81fa3a0bcea1d4cf7
Doug MacEachern <dougm@apache.org>
2001-11-06 18:43:57 +0000

    add tests for $r->printf
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68821 13f79535-47bb-0310-9956-ffa450edef68

---
+++ t/response/TestModperl/printf.pm
@@ -0,0 +1,30 @@
+package TestModperl::printf;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Apache::RequestIO ();
+
+sub handler {
+    my $r = shift;
+
+    my $tests = 3;
+
+    $r->printf("1..%d\n", $tests);
+
+    $r->printf("ok");
+
+    $r->printf(" %d\n", 1);
+
+    my $fmt = "%s%s %d\n";
+    $r->printf($fmt, qw(o k), 2);
+
+    my @a = ("ok %d%c", 3, ord("\n"));
+    $r->PRINTF(@a);
+
+    Apache::OK;
+}
+
+1;
+__END__
+
