e4aa2be10bc576aeeaafe3feed7b9b918cb701b3
Doug MacEachern <dougm@apache.org>
2001-11-13 18:09:19 +0000

    giving pnotes its own test
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68844 13f79535-47bb-0310-9956-ffa450edef68

---
+++ t/response/TestModperl/pnotes.pm
@@ -0,0 +1,37 @@
+package TestModperl::pnotes;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Apache::Test;
+use Apache::TestUtil;
+
+use Apache::Const -compile => 'OK';
+
+sub handler {
+    my $r = shift;
+
+    plan $r, tests => 5;
+
+    ok $r->pnotes;
+
+    ok t_cmp('pnotes_bar',
+             $r->pnotes('pnotes_foo', 'pnotes_bar'),
+             q{$r->pnotes(key,val)});
+
+    ok t_cmp('pnotes_bar',
+             $r->pnotes('pnotes_foo'),
+             q{$r->pnotes(key)});
+
+    ok t_cmp('HASH', ref($r->pnotes), q{ref($r->pnotes)});
+
+    ok t_cmp('pnotes_bar', $r->pnotes()->{'pnotes_foo'},
+             q{$r->pnotes()->{}});
+
+    Apache::OK;
+}
+
+1;
+__END__
+
+
