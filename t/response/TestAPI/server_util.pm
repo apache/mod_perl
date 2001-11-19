c5803db8f5dfee3a483385070abfc28be2b339c3
Doug MacEachern <dougm@apache.org>
2001-11-19 23:47:24 +0000

    add tests for Apache::server_root constant
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68867 13f79535-47bb-0310-9956-ffa450edef68

--- t/response/TestAPI/server_util.pm
+++ t/response/TestAPI/server_util.pm
@@ -14,7 +14,7 @@ sub handler {
 
     my $s = $r->server;
 
-    plan $r, tests => 7;
+    plan $r, tests => 9;
 
     for my $p ($r->pool, $r->connection->pool,
                $r, $r->connection, $r->server)
@@ -24,7 +24,15 @@ sub handler {
         ok -d $dir;
     }
 
-    my $dir = Apache->server_root_relative('logs'); #1.x ish
+    my $dir = Apache::server_root; #constant
+
+    ok -d $dir;
+
+    $dir = join '/', Apache::server_root, 'logs';
+
+    ok $dir eq Apache::server_root_relative($r->pool, 'logs');
+
+    $dir = Apache->server_root_relative('logs'); #1.x ish
 
     ok -d $dir;
 
