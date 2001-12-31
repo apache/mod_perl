895537cefc7402459fbe21200af3f51985fc6206
Stas Bekman <stas@apache.org>
2001-12-31 12:12:54 +0000

    apr sources adjustment: new argument 'mech' added to apr_lock_create PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@68965 13f79535-47bb-0310-9956-ffa450edef68

--- xs/APR/Lock/APR__Lock.h
+++ xs/APR/Lock/APR__Lock.h
@@ -3,10 +3,11 @@
 static MP_INLINE apr_lock_t *mpxs_apr_lock_create(pTHX_ SV *CLASS,
                                                   apr_pool_t *p,
                                                   apr_locktype_e type,
+                                                  apr_lockmech_e mech,
                                                   apr_lockscope_e scope,
                                                   const char *fname)
 {
     apr_lock_t *retval=NULL;
-    (void)apr_lock_create(&retval, type, scope, fname, p);
+    (void)apr_lock_create(&retval, type, mech, scope, fname, p);
     return retval;
 }
