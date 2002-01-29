f6aa47f4a7bcf8a00da35300f0d90d817e2a72ec
Stas Bekman <stas@apache.org>
2002-01-29 07:47:25 +0000

    - implement APR::String::format_size as a wrapper to apr_strfsize PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69011 13f79535-47bb-0310-9956-ffa450edef68

---
+++ t/response/TestAPR/string.pm
@@ -0,0 +1,35 @@
+package TestAPR::string;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Apache::Test;
+use Apache::TestUtil;
+use Apache::Const -compile => 'OK';
+
+use APR::String ();
+
+my %size_string = (
+    '-1'            => "  - ",
+    0               => "  0 ",
+    42              => " 42 ",
+    42_000          => " 41K",
+    42_000_000      => " 40M",
+#    42_000_000_000   => "40G",
+);
+
+sub handler {
+    my $r = shift;
+
+    plan $r, tests => scalar keys %size_string;
+
+    t_debug("size_string");
+    while (my($k, $v) = each %size_string) {
+        ok t_cmp($v, APR::String::format_size($k));
+    }
+
+    Apache::OK;
+}
+
+1;
+__END__
