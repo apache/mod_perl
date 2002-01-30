6c7f5c256937f75dd78d91f0424803c8b52eb052
Doug MacEachern <dougm@apache.org>
2002-01-30 03:30:24 +0000

    conn_rec.client_socket has gone away again
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69019 13f79535-47bb-0310-9956-ffa450edef68

--- xs/Apache/Connection/Apache__Connection.h
+++ xs/Apache/Connection/Apache__Connection.h
@@ -2,8 +2,6 @@ static MP_INLINE
 apr_socket_t *mpxs_Apache__Connection_client_socket(pTHX_ conn_rec *c,
                                                     apr_socket_t *s)
 {
-    /* XXX: until minds are made up */
-#if 0
     apr_socket_t *socket =
         ap_get_module_config(c->conn_config, &core_module);
 
@@ -12,11 +10,4 @@ apr_socket_t *mpxs_Apache__Connection_client_socket(pTHX_ conn_rec *c,
     }
 
     return socket;
-#else
-    if (s) {
-        c->client_socket = s;
-    }
-
-    return c->client_socket;
-#endif
 }
