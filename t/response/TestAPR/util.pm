e35892fc71c3db020ceb9054db83d7deaa2f97db
Stas Bekman <stas@apache.org>
2002-01-31 02:41:27 +0000

    - backoff my change from APR:: to APR::Util:: in the mapping of functions generate_random_bytes, password_validate, strerror PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69023 13f79535-47bb-0310-9956-ffa450edef68

--- t/response/TestAPR/util.pm
+++ t/response/TestAPR/util.pm
@@ -14,14 +14,14 @@ sub handler {
     plan $r, tests => 3;
 
     my $blen = 10;
-    my $bytes = APR::Util::generate_random_bytes(10);
+    my $bytes = APR::generate_random_bytes(10);
     ok length($bytes) == $blen;
 
-    my $status = APR::Util::password_validate("one", "two");
+    my $status = APR::password_validate("one", "two");
 
     ok $status != 0;
 
-    my $str= APR::Util::strerror($status);
+    my $str= APR::strerror($status);
 
     t_debug "strerror=$str\n";
 
