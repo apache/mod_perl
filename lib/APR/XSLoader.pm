3813f11ae90be839fa0ee905f780d8f289fee7a3
Doug MacEachern <dougm@apache.org>
2002-04-16 23:29:18 +0000

    PR: Obtained from: Submitted by: Reviewed by: workaround bug in 5.6.1 when XSLoader loads DynaLoader, wiping out any dl handles it had been keeping track of.
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69128 13f79535-47bb-0310-9956-ffa450edef68

--- lib/APR/XSLoader.pm
+++ lib/APR/XSLoader.pm
@@ -3,6 +3,7 @@ package APR::XSLoader;
 use strict;
 use warnings FATAL => 'all';
 
+use DynaLoader (); #XXX workaround for 5.6.1 bug
 use XSLoader ();
 
 BEGIN {
--- lib/Apache/XSLoader.pm
+++ lib/Apache/XSLoader.pm
@@ -3,6 +3,7 @@ package Apache::XSLoader;
 use strict;
 use warnings FATAL => 'all';
 
+use DynaLoader (); #XXX workaround for 5.6.1 bug
 use XSLoader ();
 
 BEGIN {
