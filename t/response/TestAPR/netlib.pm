495aa50aaab2b7da5af5d6b728784564e654a9f7
Doug MacEachern <dougm@apache.org>
2002-05-13 02:49:05 +0000

    fix for win32
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69153 13f79535-47bb-0310-9956-ffa450edef68

--- t/response/TestAPR/netlib.pm
+++ t/response/TestAPR/netlib.pm
@@ -33,12 +33,18 @@ sub handler {
     ok $ipsub->test($c->remote_addr);
 
     my $reverse_remote_ip = scalar reverse $ip;
-    $ipsub = APR::IpSubnet->new($p, $reverse_remote_ip);
 
     ok t_cmp($reverse_remote_ip, scalar reverse($c->remote_addr->ip_get),
              "reversed remote_ip eq reversed remote_addr->ip_get");
 
-    ok ! $ipsub->test($c->remote_addr);
+    $ipsub = APR::IpSubnet->new($p, $reverse_remote_ip);
+
+    if (!$ipsub) {
+        ok 1; #this happens on win32
+    }
+    else {
+        ok ! $ipsub->test($c->remote_addr);
+    }
 
     Apache::OK;
 }
