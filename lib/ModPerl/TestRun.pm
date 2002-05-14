4da5d2abdc3ec8dd32ece51eb7b69e5109fea580
Doug MacEachern <dougm@apache.org>
2002-05-14 23:14:11 +0000

    subclass of Apache::TestRun so LoadModule of mod_perl.so is not inherited
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69174 13f79535-47bb-0310-9956-ffa450edef68

---
+++ lib/ModPerl/TestRun.pm
@@ -0,0 +1,26 @@
+package ModPerl::TestRun;
+
+use Apache::TestRunPerl ();
+
+our @ISA = qw(Apache::TestRunPerl);
+
+sub new_test_config {
+    my $self = shift;
+
+    ModPerl::TestConfig->new($self->{conf_opts});
+}
+
+package ModPerl::TestConfig;
+
+our @ISA = qw(Apache::TestConfig);
+
+#don't inherit LoadModule perl_module from the apache httpd.conf
+
+sub should_load_module {
+    my($self, $name) = @_;
+
+    $name eq 'mod_perl.c' ? 0 : 1;
+}
+
+1;
+
