c59e6fe61f8e477b0702c233d6b712a4ee84e009
Stas Bekman <stas@apache.org>
2002-05-14 17:12:01 +0000

    registry loader is a bit broken, disable the loading at the startup for now PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69172 13f79535-47bb-0310-9956-ffa450edef68

--- ModPerl-Registry/t/conf/modperl_extra_startup.pl
+++ ModPerl-Registry/t/conf/modperl_extra_startup.pl
@@ -1,41 +1,47 @@
 # 
-
 use strict;
-use warnings;
+use warnings FATAL => 'all';
 
+# XXX: this should go
+use Apache::compat;
+
+use Apache::ServerUtil;
+use Apache::Process;
+use APR::Pool;
 # test the scripts pre-loading by explicitly specifying uri => filename
 use ModPerl::RegistryLoader ();
 my $rl = ModPerl::RegistryLoader->new(package => "ModPerl::Registry");
-my $base_dir = Apache::server_root_relative("cgi-bin");
+
+my $pool = Apache->server->process->pool;
+my $base_dir = Apache::server_root_relative($pool, "cgi-bin");
 my $base_uri = "/cgi-bin";
-for my $file (qw(basic.pl env.pl)) {
-    my $file_path  = "$base_dir/$file";
-    my $info_path  = "$base_uri/$file";
-    $rl->handler($info_path, $file_path);
-}
+#for my $file (qw(basic.pl env.pl)) {
+#    my $file_path  = "$base_dir/$file";
+#    my $info_path  = "$base_uri/$file";
+#    $rl->handler($info_path, $file_path);
+#}
 
-{
-    # test the scripts pre-loading by using trans sub
-    use ModPerl::RegistryLoader ();
-    use DirHandle ();
-    use strict;
+#{
+#    # test the scripts pre-loading by using trans sub
+#    use DirHandle ();
+#    use strict;
 
-    my $dir = Apache::server_root_relative("cgi-bin");
+#    sub trans {
+#        my $uri = shift; 
+#        $uri =~ s|^/registry_bb/|cgi-bin/|;
+#        return Apache::server_root_relative($pool, $uri);
+#    }
 
-    sub trans {
-        my $uri = shift; 
-        $uri =~ s|^/registry_bb/|cgi-bin/|;
-        return Apache::server_root_relative($uri);
-    }
+#    my $dir = Apache::server_root_relative($pool, "cgi-bin");
 
-    my $rl = ModPerl::RegistryLoader->new(package => "ModPerl::RegistryBB",
-                                          trans   => \&trans);
-    my $dh = DirHandle->new($dir) or die $!;
+#    my $rl = ModPerl::RegistryLoader->new(package => "ModPerl::RegistryBB",
+#                                          trans   => \&trans);
+#    my $dh = DirHandle->new($dir) or die $!;
 
-    for my $file ($dh->read) {
-        next unless $file =~ /\.pl$/;
-        $rl->handler("/registry_bb/$file");
-    }
-}
+#    for my $file ($dh->read) {
+#        next unless $file =~ /\.pl$/;
+#        $rl->handler("/registry_bb/$file");
+#    }
+#}
 
 1;
