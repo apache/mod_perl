18d2a6f41580202357e75a59b706e80806b3e6f7
Doug MacEachern <dougm@apache.org>
2002-05-19 20:09:27 +0000

    wrapper for $r->content_type
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69204 13f79535-47bb-0310-9956-ffa450edef68

---
+++ xs/Apache/RequestRec/Apache__RequestRec.h
@@ -0,0 +1,14 @@
+static MP_INLINE
+const char *mpxs_Apache__RequestRec_content_type(pTHX_ request_rec *r,
+                                                 SV *type)
+{
+    const char *retval = r->content_type;
+
+    if (type) {
+        STRLEN len;
+        const char *val = SvPV(type, len);
+        ap_set_content_type(r, apr_pmemdup(r->pool, val, len));
+    }
+
+    return retval;
+}
