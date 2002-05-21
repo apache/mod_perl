30ea06729e43c329af08eeaea42fed7d7663227d
Doug MacEachern <dougm@apache.org>
2002-05-21 00:22:24 +0000

    add support for $r->proxyreq to detect proxy requests
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69211 13f79535-47bb-0310-9956-ffa450edef68

--- xs/Apache/RequestRec/Apache__RequestRec.h
+++ xs/Apache/RequestRec/Apache__RequestRec.h
@@ -12,3 +12,30 @@ const char *mpxs_Apache__RequestRec_content_type(pTHX_ request_rec *r,
 
     return retval;
 }
+
+static MP_INLINE
+int mpxs_Apache__RequestRec_proxyreq(pTHX_ request_rec *r, SV *val)
+{
+    int retval = r->proxyreq;
+
+    if (!val && !r->proxyreq &&
+        r->parsed_uri.scheme &&
+	!(r->parsed_uri.hostname && 
+	  strEQ(r->parsed_uri.scheme, ap_http_method(r)) &&
+	  ap_matches_request_vhost(r, r->parsed_uri.hostname,
+                                   r->parsed_uri.port_str ? 
+                                   r->parsed_uri.port : 
+                                   ap_default_port(r))))
+    {
+        retval = r->proxyreq = 1;
+        r->uri = r->unparsed_uri;
+        /* else mod_proxy will segfault */
+        r->filename = apr_pstrcat(r->pool, "modperl-proxy:", r->uri, NULL);
+    }
+
+    if (val) {
+        r->proxyreq = SvIV(val);
+    }
+
+    return retval;
+}
