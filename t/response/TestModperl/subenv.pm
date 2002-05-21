abf7550ae07c025eb51712918c96000e871b7816
Doug MacEachern <dougm@apache.org>
2002-05-21 02:50:39 +0000

    tests for $r->subprocess_env
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69214 13f79535-47bb-0310-9956-ffa450edef68

---
+++ t/response/TestModperl/subenv.pm
@@ -0,0 +1,72 @@
+package TestModperl::subenv;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Apache::RequestRec ();
+use APR::Table ();
+
+use Apache::Test;
+
+use Apache::Const -compile => 'OK';
+
+sub handler {
+    my $r = shift;
+
+    plan $r, tests => 16;
+
+    my $env = $r->subprocess_env;
+
+    ok $env;
+
+    ok_false($r, 'REMOTE_ADDR');
+
+    $r->subprocess_env; #void context populates
+
+    $env = $r->subprocess_env; #table may have been overlayed
+
+    ok_true($r, 'REMOTE_ADDR');
+
+    $env->set(FOO => 1);
+
+    ok_true($r, 'FOO');
+
+    $r->subprocess_env(FOO => undef);
+
+    ok_false($r, 'FOO');
+
+    $r->subprocess_env(FOO => 1);
+
+    ok_true($r, 'FOO');
+
+    Apache::OK;
+}
+
+sub ok_true {
+    my($r, $key) = @_;
+
+    my $env = $r->subprocess_env;
+
+    ok $env->get($key);
+
+    ok $env->{$key};
+
+    ok $r->subprocess_env($key);
+}
+
+sub ok_false {
+    my($r, $key) = @_;
+
+    my $env = $r->subprocess_env;
+
+    ok ! $env->get($key);
+
+    ok ! $env->{$key};
+
+    ok ! $r->subprocess_env($key);
+}
+
+1;
+__END__
+PerlOptions -SetupEnv
+
