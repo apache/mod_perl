960c47bcffc5c3d4f2589aadd6dd5daeb0e60678
Doug MacEachern <dougm@apache.org>
2002-06-16 01:07:21 +0000

    new module where any PL_ppaddr modifications are done
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69330 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/modules/perl/modperl_perl_pp.c
@@ -0,0 +1,91 @@
+#include "mod_perl.h"
+
+static enum opcode MP_pp_map[] = {
+#ifdef MP_REFGEN_FIXUP
+    OP_SREFGEN,
+#endif
+};
+
+typedef OP * MP_FUNC_T(modperl_pp_t)(pTHX);
+
+static modperl_pp_t MP_PERL_ppaddr[MP_OP_max];
+
+#ifdef MP_REFGEN_FIXUP
+
+/*
+ * nasty workaround for bug fixed in bleedperl (11536 + 11553)
+ * XXX: when 5.8.0 is released + stable, we will require 5.8.0
+ * if ithreads are enabled.
+ */
+
+static OP *modperl_pp_srefgen(pTHX)
+{
+    dSP;
+    OP *o;
+    SV *sv = *SP;
+
+    if (SvPADTMP(sv) && IS_PADGV(sv)) {
+        /* prevent S_refto from making a copy of the GV,
+         * tricking it to SvREFCNT_inc and point to this one instead.
+         */
+        SvPADTMP_off(sv);
+    }
+    else {
+        sv = Nullsv;
+    }
+
+    /* o = Perl_pp_srefgen(aTHX) */
+    o = MP_PERL_ppaddr[MP_OP_SREFGEN](aTHX);
+
+    if (sv) {
+        /* restore original flags */
+        SvPADTMP_on(sv);
+    }
+
+    return o;
+}
+
+#endif /* MP_REFGEN_FIXUP */
+
+static modperl_pp_t MP_ppaddr[] = {
+#ifdef MP_REFGEN_FIXUP
+    MEMBER_TO_FPTR(modperl_pp_srefgen),
+#endif
+};
+
+void modperl_perl_pp_set(modperl_perl_opcode_e idx)
+{
+    int pl_idx = MP_pp_map[idx];
+
+    /* save original */
+    MP_PERL_ppaddr[idx] = PL_ppaddr[pl_idx];
+
+    /* replace with our own */
+    PL_ppaddr[pl_idx] = MP_ppaddr[idx];
+}
+
+void modperl_perl_pp_set_all(void)
+{
+    int i;
+
+    for (i=0; i<MP_OP_max; i++) {
+        modperl_perl_pp_set(i);
+    }
+}
+
+void modperl_perl_pp_unset(modperl_perl_opcode_e idx)
+{
+    int pl_idx = MP_pp_map[idx];
+
+    /* restore original */
+    PL_ppaddr[pl_idx] = MP_PERL_ppaddr[idx];
+}
+
+void modperl_perl_pp_unset_all(void)
+{
+    int i;
+
+    for (i=0; i<MP_OP_max; i++) {
+        modperl_perl_pp_unset(i);
+    }
+}
