f375809f3ea09718d1af2e6483ef2cdb0732f85a
Doug MacEachern <dougm@apache.org>
2002-06-29 20:38:33 +0000

    add support for redirects with PerlOptions +ParseHeaders
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69377 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/perl/modperl_cgi.c
+++ src/modules/perl/modperl_cgi.c
@@ -5,6 +5,7 @@ MP_INLINE int modperl_cgi_header_parse(request_rec *r, char *buffer,
 {
     int status;
     int termarg;
+    const char *location;
 
     if (!buffer) {
         return DECLINED;
@@ -12,6 +13,38 @@ MP_INLINE int modperl_cgi_header_parse(request_rec *r, char *buffer,
 
     status = ap_scan_script_header_err_strs(r, NULL, bodytext,
                                             &termarg, buffer, NULL);
+
+    /* code below from mod_cgi.c */
+    location = apr_table_get(r->headers_out, "Location");
+
+    if (location && (location[0] == '/') && (r->status == 200)) {
+        r->method = apr_pstrdup(r->pool, "GET");
+        r->method_number = M_GET;
+
+        /* We already read the message body (if any), so don't allow
+         * the redirected request to think it has one.  We can ignore 
+         * Transfer-Encoding, since we used REQUEST_CHUNKED_ERROR.
+         */
+        apr_table_unset(r->headers_in, "Content-Length");
+
+        ap_internal_redirect_handler(location, r);
+
+        return OK;
+    }
+    else if (location && (r->status == 200)) {
+        MP_dRCFG;
+
+        /* Note that if a script wants to produce its own Redirect
+         * body, it now has to explicitly *say* "Status: 302"
+         */
+
+        /* XXX: this is a hack.
+         * filter return value doesn't seem to impact anything.
+         */
+        rcfg->status = HTTP_MOVED_TEMPORARILY;
+
+        return HTTP_MOVED_TEMPORARILY;
+    }
 
     return status;
 }
