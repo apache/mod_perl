8b5872f567fe4a2ee2e32ad5a38d6e69ee098d49
Doug MacEachern <dougm@apache.org>
2002-08-27 04:28:38 +0000

    add Apache::Module->get_config method
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69472 13f79535-47bb-0310-9956-ffa450edef68

--- xs/Apache/Module/Apache__Module.h
+++ xs/Apache/Module/Apache__Module.h
@@ -37,3 +37,47 @@ static MP_INLINE int mpxs_Apache__Module_loaded(pTHX_ char *name)
         return modperl_perl_module_loaded(aTHX_ name);
     }
 }
+
+static MP_INLINE SV *mpxs_Apache__Module_get_config(pTHX_
+                                                    SV *self,
+                                                    SV *pmodule,
+                                                    server_rec *s,
+                                                    ap_conf_vector_t *v)
+{
+    MP_dSCFG(s);
+    module *modp;
+    const char *name;
+    void *ptr;
+    PTR_TBL_t *table;
+    SV *obj;
+
+    if (!v) {
+        v = s->module_config;
+    }
+
+    if (SvROK(pmodule)) {
+        name = SvCLASS(pmodule);
+    }
+    else {
+        STRLEN n_a;
+        name = SvPV(pmodule, n_a);
+    }
+
+    if (!(modp = apr_hash_get(scfg->modules, name, APR_HASH_KEY_STRING))) {
+        return Nullsv;
+    }
+
+    if (!(ptr = ap_get_module_config(v, modp))) {
+        return Nullsv;
+    }
+
+    if (!(table = modperl_module_config_table_get(aTHX_ FALSE))) {
+        return Nullsv;
+    }
+
+    if (!(obj = ptr_table_fetch(table, ptr))) {
+        return Nullsv;
+    }
+
+    return SvREFCNT_inc(obj);
+}
