71a3db6defa1fd652276a5e21cbd1302dcd55143
Doug MacEachern <dougm@apache.org>
2002-08-29 02:28:44 +0000

    add Apache::DIR_MAGIC_TYPE constant
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69488 13f79535-47bb-0310-9956-ffa450edef68

--- xs/Apache/Const/Const.pm
+++ xs/Apache/Const/Const.pm
@@ -8,4 +8,8 @@ our @ISA = qw(ModPerl::Const);
 
 XSLoader::load(__PACKAGE__, $VERSION);
 
+#XXX: we don't support string constants in the lookup functions
+#always define this one for the moment
+sub Apache::DIR_MAGIC_TYPE () { "httpd/unix-directory" }
+
 1;
