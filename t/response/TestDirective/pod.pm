3abe06ba5d5de57ccf3838a5f4d7e7579619edf7
Doug MacEachern <dougm@apache.org>
2002-09-17 02:37:44 +0000

    PR: Obtained from: Submitted by:	Philippe M. Chiasson <gozer@cpan.org> Reviewed by:	dougm test for pod directives
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69512 13f79535-47bb-0310-9956-ffa450edef68

---
+++ t/response/TestDirective/pod.pm
@@ -0,0 +1,37 @@
+package TestDirective::pod;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Apache::Test;
+use Apache::TestUtil;
+use Apache::Const -compile => 'OK';
+
+sub handler {
+    my $r = shift;
+
+    plan $r, tests => 3;
+
+    ok t_cmp('yes', $r->dir_config->get('TestDirective__pod_over_worked'));
+    ok t_cmp('yes', $r->dir_config->get('TestDirective__pod_cut_worked'));
+
+    #XXX: How to test that __END__ works proprely without cloberring all the other tests?
+    ok t_cmp('__END__', '__END__');
+
+    Apache::OK;
+}
+
+1;
+__END__
+=pod
+This is some pod data
+
+=over apache
+PerlSetVar TestDirective__pod_over_worked yes
+=back
+
+This is some more pod
+
+=cut
+
+PerlSetVar TestDirective__pod_cut_worked yes
