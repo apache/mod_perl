4455a3e1b8af5ca9023256f9ccee308a53674754
Doug MacEachern <dougm@apache.org>
2002-10-21 17:58:40 +0000

    PR: Obtained from: Submitted by:	gozer Reviewed by:	dougm add support for setting $r->auth_name and $r->auth_type
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69530 13f79535-47bb-0310-9956-ffa450edef68

--- t/response/TestAPI/access.pm
+++ t/response/TestAPI/access.pm
@@ -12,7 +12,7 @@ use Apache::Const -compile => qw(OK :options :override :satisfy);
 sub handler {
     my $r = shift;
 
-    plan $r, tests => 8;
+    plan $r, tests => 10;
 
     $r->allow_methods(1, qw(GET POST));
 
@@ -28,7 +28,15 @@ sub handler {
 
     ok $r->auth_name eq 'modperl';
 
+    $r->auth_name('modperl_test');
+    ok $r->auth_name eq 'modperl_test';
+    $r->auth_name('modperl');
+
     ok $r->auth_type eq 'none';
+    
+    $r->auth_type('Basic');
+    ok $r->auth_type eq 'Basic';
+    $r->auth_type('none');
 
     ok !$r->some_auth_required;
 
