6d02f24c591a9aaf5cc0e9e3dea951a36257aab9
Doug MacEachern <dougm@apache.org>
2002-10-22 15:13:22 +0000

    PR: Obtained from: Submitted by:	gozer Reviewed by:	dougm default AuthType to Basic if not set in $r->get_basic_auth_pw()
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69538 13f79535-47bb-0310-9956-ffa450edef68

--- xs/Apache/Access/Apache__Access.h
+++ xs/Apache/Access/Apache__Access.h
@@ -1,28 +1,3 @@
-static XS(MPXS_ap_get_basic_auth_pw)
-{
-    dXSARGS;
-    request_rec *r;
-    const char *sent_pw = NULL;
-    int rc;
-
-    mpxs_usage_items_1("r");
-
-    mpxs_PPCODE({
-        r = mp_xs_sv2_r(ST(0));
-
-        rc = ap_get_basic_auth_pw(r, &sent_pw);
-
-        EXTEND(SP, 2);
-        PUSHs_mortal_iv(rc);
-        if (rc == OK) {
-            PUSHs_mortal_pv(sent_pw);
-        }
-        else {
-            PUSHs(&PL_sv_undef);
-        }
-    });
-}
-
 static MP_INLINE SV *mpxs_ap_requires(pTHX_ request_rec *r)
 {
     AV *av;
@@ -119,4 +94,34 @@ const char *mpxs_Apache__RequestRec_auth_name(pTHX_ request_rec *r,
     }
 
     return ap_auth_name(r);
+}
+
+static XS(MPXS_ap_get_basic_auth_pw)
+{
+    dXSARGS;
+    request_rec *r;
+    const char *sent_pw = NULL;
+    int rc;
+
+    mpxs_usage_items_1("r");
+
+    mpxs_PPCODE({
+        r = mp_xs_sv2_r(ST(0));
+
+        /* Default auth-type to Basic */
+        if (!ap_auth_type(r)) {
+            mpxs_Apache__RequestRec_auth_type(aTHX_ r, "Basic");
+        }
+
+        rc = ap_get_basic_auth_pw(r, &sent_pw);
+
+        EXTEND(SP, 2);
+        PUSHs_mortal_iv(rc);
+        if (rc == OK) {
+            PUSHs_mortal_pv(sent_pw);
+        }
+        else {
+            PUSHs(&PL_sv_undef);
+        }
+    });
 }
