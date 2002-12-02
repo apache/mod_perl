16f6c02a8a41e644a279ffea00875a6fc14c5be4
Stas Bekman <stas@apache.org>
2002-12-02 16:22:09 +0000

    Move the custom mod_perl 2.0 configuration bits out of the ModPerl::TestRun, where they don't belong, into a special config file which is included at the very end of httpd.conf PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69564 13f79535-47bb-0310-9956-ffa450edef68

--- lib/ModPerl/TestRun.pm
+++ lib/ModPerl/TestRun.pm
@@ -22,57 +22,5 @@ sub should_load_module {
     $name eq 'mod_perl.c' ? 0 : 1;
 }
 
-sub configure_startup_pl {
-    my $self = shift;
-
-    $self->SUPER::configure_startup_pl;
-
-    #XXX: issue for these is they need to happen after PerlSwitches
-
-    #XXX: this should only be done for the modperl-2.0 tests
-    my $htdocs = $self->{vars}{documentroot};
-    $self->postamble(<<"EOF");
-<Perl >
-push \@Alias, ['/perl_sections', '$htdocs'],
-\$Location{'/perl_sections'} = {
-	'PerlInitHandler' => 'ModPerl::Test::add_config',
-	'AuthType' => 'Basic',
-	'AuthName' => 'PerlSection',
-	'PerlAuthenHandler' => 'TestHooks::authen',
-	};
-</Perl>
-EOF
-
-    #XXX: this should only be done for the modperl-2.0 tests
-    $self->postamble(<<'EOF');
-PerlLoadModule TestDirective::loadmodule
-
-MyTest one two
-ServerTest per-server
-
-<Location /TestDirective::loadmodule>
-    MyOtherTest value
-</Location>
-EOF
-
-	#XXX: this should only be done for the modperl-2.0 tests
-	$self->postamble(<<'EOF');
-	Perl $TestDirective::perl::worked="yes";
-EOF
-
-    #XXX: this should only be done for the modperl-2.0 tests
-    $self->postamble(<<'EOF');
-=pod
-This is some pod data
-=over apache
-PerlSetVar TestDirective__pod_over_worked yes
-=back
-This is some more pod
-=cut
-PerlSetVar TestDirective__pod_cut_worked yes
-EOF
-    
-}
-
 1;
 
