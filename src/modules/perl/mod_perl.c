267743689fe9efe4d9a4a706ad6eae5e993be74c
Stas Bekman <stas@apache.org>
2003-01-23 04:09:12 +0000

    more the rc declaration to the head of the function where it belongs PR: Obtained from: Submitted by:	Randy Kobes <randy@theoryx5.uwinnipeg.ca> Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69691 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/perl/mod_perl.c
+++ src/modules/perl/mod_perl.c
@@ -752,8 +752,8 @@ int modperl_response_handler_cgi(request_rec *r)
 {
     MP_dDCFG;
     GV *h_stdin, *h_stdout;
-    int retval;
-    MP_dRCFG;
+    apr_status_t retval, rc;
+ MP_dRCFG;
 #ifdef USE_ITHREADS
     pTHX;
     modperl_interp_t *interp;
@@ -806,7 +806,7 @@ int modperl_response_handler_cgi(request_rec *r)
 #endif
 
     /* flush output buffer after interpreter is putback */
-    apr_status_t rc = modperl_response_finish(r);
+    rc = modperl_response_finish(r);
     if (rc != APR_SUCCESS) {
         retval = rc;
     }
