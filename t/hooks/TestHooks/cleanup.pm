07e4929b69672487bcf6912d116c867fc50c061e
Stas Bekman <stas@apache.org>
2003-02-03 06:40:33 +0000

    PerlCleanupHandler implementation + tests PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69723 13f79535-47bb-0310-9956-ffa450edef68

---
+++ t/hooks/TestHooks/cleanup.pm
@@ -0,0 +1,61 @@
+package TestHooks::cleanup;
+
+# test various ways to push handlers
+
+use strict;
+use warnings FATAL => 'all';
+
+use Apache::Test;
+use Apache::TestUtil;
+
+use File::Spec::Functions qw(catfile catdir);
+
+use Apache::RequestRec ();
+use Apache::RequestIO ();
+use Apache::RequestUtil ();
+
+use Apache::Const -compile => qw(OK DECLINED);
+
+sub get_file {
+    catdir Apache::Test::config->{vars}->{documentroot}, "hooks", "cleanup";
+}
+
+sub handler {
+    my $r = shift;
+
+    $r->content_type('text/plain');
+    $r->print('ok');
+
+    $r->push_handlers(PerlCleanupHandler => \&cleanup2);
+
+    return Apache::OK;
+}
+
+sub cleanup1 {
+    my $r = shift;
+
+    #warn "cleanup CALLED\n";
+    t_write_file(get_file(), "cleanup");
+
+    return Apache::OK;
+}
+
+sub cleanup2 {
+    my $r = shift;
+
+    #warn "cleanup2 CALLED\n";
+    t_append_file(get_file(), " ok");
+
+    return Apache::OK;
+}
+
+1;
+__DATA__
+<NoAutoConfig>
+  <Location /TestHooks::cleanup>
+      SetHandler modperl
+      PerlCleanupHandler  TestHooks::cleanup::cleanup1
+      PerlResponseHandler TestHooks::cleanup
+  </Location>
+</NoAutoConfig>
+
