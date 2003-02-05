7899311f27643e2439447f2f54d235ecd26e6850
Stas Bekman <stas@apache.org>
2003-02-05 04:10:05 +0000

    verify that scripts generating partial content headers are working correctly
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69730 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ModPerl-Registry/t/cgi-bin/206.pl
@@ -0,0 +1,10 @@
+       print <<_OUT_;
+Status: 206 Partial Content
+Content-Type: text/html; charset=UTF-8
+Content-Length: 11
+Content-Range: bytes 0-10/1336
+Date: Fri, 31 Jan 2003 09:39:01 GMT
+ETag: "xxxx"
+
+_OUT_
+print '<?xml versi';
