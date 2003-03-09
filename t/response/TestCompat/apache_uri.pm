99fac72d81569947fa116b292c583163a4042447
Stas Bekman <stas@apache.org>
2003-03-09 08:57:55 +0000

    - fixes for apr_uri_unparse, which has the segfault resolved in apr-0.9.2, but is not using the default 'http' scheme if such is not provided. So we have to do special overriding in Apache::compat to keep the mp1-like behavior. - adjust/add tests PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69803 13f79535-47bb-0310-9956-ffa450edef68

---
+++ t/response/TestCompat/apache_uri.pm
@@ -0,0 +1,61 @@
+package TestCompat::apache_uri;
+
+# Apache::Util compat layer tests
+
+# these tests are all run and validated on the server side.
+
+use strict;
+use warnings FATAL => 'all';
+
+use Apache::TestUtil;
+use Apache::Test;
+
+use Apache::compat ();
+use Apache::Constants qw(OK);
+
+
+
+
+sub handler {
+    my $r = shift;
+
+    plan $r, tests => 1;
+
+    # XXX: need to test ->parse
+    #    {
+    #        my @methods = qw(scheme hostinfo user password hostname path rpath
+    #                         query fragment port unparse);
+    #        my $test_uri = "http://foo:bar@perl.apache.org:80/docs/index.html";
+    # 
+    #        for my $uri ($r->parsed_uri, Apache::URI->parse($r, $test_uri)) {
+    #            t_debug("URI=" . $uri->unparse);
+    #            no strict 'refs';
+    #            for my $meth (@methods) {
+    #                my $val = $uri->$meth();
+    #                t_debug("$meth: $val");
+    #                ok $val || 1;
+    #            }
+    #        }
+    #    }
+
+    {
+        # since Apache::compat redefines APR::URI::unparse and the test for
+        # real APR::URI forces reload of APR::URI (to get the right behavior),
+        # we need to force reload of Apache::compat
+        delete $INC{"Apache/compat.pm"};
+        require Apache::compat;
+
+         # test the segfault in apr < 0.9.2 (fixed on mod_perl side)
+        # passing only the /path
+        my $parsed = APR::URI->parse($r->pool, $r->uri);
+        # set hostname, but not the scheme
+        $parsed->hostname($r->get_server_name);
+        $parsed->port($r->get_server_port);
+        #$parsed->scheme('http'); # compat defaults to 'http' like apache-1.3 did
+        ok t_cmp($r->construct_url, $parsed->unparse);
+    }
+
+    OK;
+}
+
+1;
--- xs/APR/URI/APR__URI.h
+++ xs/APR/URI/APR__URI.h
@@ -4,11 +4,15 @@ char *mpxs_apr_uri_unparse(pTHX_
                            unsigned flags)
 {
 
-    /* XXX: check that my patch was actually applied in apr v9.3 */
-#if APR_MINOR_VERSION == 9 && APR_PATCH_VERSION < 3
-    /* apr < 0.9.3 segfaults if hostname is set, but scheme is not */
+    /* apr =< 0.9.2-dev segfaults if hostname is set, but scheme is not.
+     * apr >= 0.9.2 simply uses "", which will force the user to set scheme
+     * since apr_uri_unparse is protocol-agnostic, it doesn't use
+     * 'http' as the default fallback anymore. so we use the same solution
+     */
+#if APR_MAJOR_VERSION == 0 && APR_MINOR_VERSION == 9 && \
+    (APR_PATCH_VERSION < 2 || APR_PATCH_VERSION == 2 && defined APR_IS_DEV_VERSION)
     if (uptr->hostname && !uptr->scheme) {
-        uptr->scheme = "http";
+        uptr->scheme = "";
     }
 #endif
     
