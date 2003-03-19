ce288efa2abb1a774ca80401adefa2278e67cb7b
Stas Bekman <stas@apache.org>
2003-03-19 02:51:59 +0000

    add more details regarding overriding  dl_load_flags in a crossplatform way PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69826 13f79535-47bb-0310-9956-ffa450edef68

--- xs/APR/APR/APR.pm
+++ xs/APR/APR/APR.pm
@@ -5,6 +5,7 @@ our $VERSION = '0.01';
 our @ISA = qw(DynaLoader);
 
 #dlopen("APR.so", RTDL_GLOBAL); so we only need to link libapr.a once
+# XXX: see xs/ModPerl/Const/Const.pm for issues of using 0x01
 sub dl_load_flags { 0x01 }
 
 unless (defined &APR::XSLoader::BOOTSTRAP) {
