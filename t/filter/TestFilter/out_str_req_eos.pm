2a4f51e7a766dd1ba3c49582f311a875229d5228
Stas Bekman <stas@apache.org>
2003-04-03 05:51:23 +0000

    this new test exposes a bug in streaming filter eos handling when more than one mp stream filter is used. the fix is coming soon.
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69875 13f79535-47bb-0310-9956-ffa450edef68

---
+++ t/filter/TestFilter/out_str_req_eos.pm
@@ -0,0 +1,78 @@
+package TestFilter::out_str_req_eos;
+
+# here we test how EOS is passed from one streaming filter to another,
+# making sure that it's not lost
+
+use strict;
+use warnings FATAL => 'all';
+
+use Apache::Filter ();
+
+use Apache::Const -compile => qw(OK M_POST);
+
+my $prefix = 'PREFIX_';
+my $suffix = '_SUFFIX';
+
+sub add_prefix {
+    my $filter = shift;
+
+    #warn "add_prefix called\n";
+
+    unless ($filter->ctx) {
+        $filter->print($prefix);
+        $filter->ctx(1);
+    }
+
+    while ($filter->read(my $buffer, 1024)){
+        #warn "add_prefix read: [$buffer]\n";
+        $filter->print($buffer);
+    }
+
+    Apache::OK;
+}
+
+sub add_suffix {
+    my $filter = shift;
+
+    #warn "add_suffix called\n";
+
+    while ($filter->read(my $buffer, 1024)){
+        #warn "add_suffix read: [$buffer]\n";
+        $filter->print($buffer);
+    }
+
+    if ($filter->seen_eos) {
+        $filter->print($suffix);
+        $filter->ctx(1);
+    }
+
+    Apache::OK;
+}
+
+sub handler {
+    my $r = shift;
+
+    $r->content_type('text/plain');
+
+    if ($r->method_number == Apache::M_POST) {
+        $r->print(ModPerl::Test::read_post($r));
+    }
+
+    return Apache::OK;
+}
+
+1;
+__DATA__
+<NoAutoConfig>
+    PerlModule TestFilter::out_str_req_eos
+    <Location /TestFilter::out_str_req_eos>
+        SetHandler modperl
+        PerlResponseHandler     TestFilter::out_str_req_eos
+        PerlOutputFilterHandler TestFilter::out_str_req_eos::add_prefix
+        PerlOutputFilterHandler TestFilter::out_str_req_eos::add_suffix
+    </Location>
+</NoAutoConfig>
+
+
+
+
