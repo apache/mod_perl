83c3e6b628d85caeb66f102756874f32334acb61
Stas Bekman <stas@apache.org>
2003-04-24 04:28:12 +0000

    $socket->timeout_(get|set) is available only since 2.0.40 (apr 0.9.3) PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69944 13f79535-47bb-0310-9956-ffa450edef68

--- t/response/TestAPR/socket.pm
+++ t/response/TestAPR/socket.pm
@@ -13,26 +13,36 @@ use APR::Socket ();
 use Apache::Const -compile => 'OK';
 use APR::Const -compile => 'EMISMATCH';
 
+use constant HAVE_APACHE_2_0_40 => have_min_apache_version("2.0.40");
+
 sub handler {
     my $r = shift;
 
-    plan $r, tests => 3;
+    my $tests = 1;
+    $tests += 3 if HAVE_APACHE_2_0_40;
+
+    plan $r, tests => $tests;
 
     my $c = $r->connection;
     my $socket = $c->client_socket;
 
-    # in microseconds
-    my $orig_val = $socket->timeout_get();
-    t_debug "orig timeout was: $orig_val";
-    ok $orig_val;
+    ok $socket;
 
-    my $new_val = 30_000_000; # 30 secs
-    $socket->timeout_set($new_val);
-    ok t_cmp($new_val, $socket->timeout_get(), "timeout_get()");
 
-    # reset the timeout
-    $socket->timeout_set($orig_val);
-    ok t_cmp($orig_val, $socket->timeout_get(), "timeout_get()");
+    if (HAVE_APACHE_2_0_40) {
+        # in microseconds
+        my $orig_val = $socket->timeout_get();
+        t_debug "orig timeout was: $orig_val";
+        ok $orig_val;
+
+        my $new_val = 30_000_000; # 30 secs
+        $socket->timeout_set($new_val);
+        ok t_cmp($new_val, $socket->timeout_get(), "timeout_get()");
+
+        # reset the timeout
+        $socket->timeout_set($orig_val);
+        ok t_cmp($orig_val, $socket->timeout_get(), "timeout_get()");
+    }
 
     Apache::OK;
 }
