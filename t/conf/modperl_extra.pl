e5c104359064046355bab1a17d2d940b6f69add7
Stas Bekman <stas@apache.org>
2003-05-14 02:26:53 +0000

    make the filter snooping functionality available for filter tests PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69972 13f79535-47bb-0310-9956-ffa450edef68

--- t/conf/modperl_extra.pl
+++ t/conf/modperl_extra.pl
@@ -129,4 +129,80 @@ END {
     warn "END in modperl_extra.pl, pid=$$\n";
 }
 
+package ModPerl::TestFilterDebug;
+
+use base qw(Apache::Filter);
+use Apache::FilterRec ();
+use APR::Brigade ();
+use APR::Bucket ();
+
+use Apache::Const -compile => qw(OK DECLINED);
+use APR::Const -compile => ':common';
+
+# to use these functions add any or all of these filter handlers
+# PerlInputFilterHandler  ModPerl::TestFilterDebug::snoop_request
+# PerlInputFilterHandler  ModPerl::TestFilterDebug::snoop_connection
+# PerlOutputFilterHandler ModPerl::TestFilterDebug::snoop_request
+# PerlOutputFilterHandler ModPerl::TestFilterDebug::snoop_connection
+#
+
+sub snoop_connection : FilterConnectionHandler { snoop("connection", @_) }
+sub snoop_request    : FilterRequestHandler    { snoop("request",    @_) }
+
+sub snoop {
+    my $type = shift;
+    my($filter, $bb, $mode, $block, $readbytes) = @_; # filter args
+
+    # $mode, $block, $readbytes are passed only for input filters
+    my $stream = defined $mode ? "input" : "output";
+
+    # read the data and pass-through the bucket brigades unchanged
+    if (defined $mode) {
+        # input filter
+        my $rv = $filter->next->get_brigade($bb, $mode, $block, $readbytes);
+        return $rv unless $rv == APR::SUCCESS;
+        bb_dump($type, $stream, $bb);
+    }
+    else {
+        # output filter
+        bb_dump($type, $stream, $bb);
+        my $rv = $filter->next->pass_brigade($bb);
+        return $rv unless $rv == APR::SUCCESS;
+    }
+    #if ($bb->empty) {
+    #    return -1;
+    #}
+
+    return Apache::OK;
+}
+
+sub bb_dump {
+    my($type, $stream, $bb) = @_;
+
+    my @data;
+    for (my $b = $bb->first; $b; $b = $bb->next($b)) {
+        $b->read(my $bdata);
+        $bdata = '' unless defined $bdata;
+        push @data, $b->type->name, $bdata;
+    }
+
+    # send the sniffed info to STDERR so not to interfere with normal
+    # output
+    my $direction = $stream eq 'output' ? ">>>" : "<<<";
+    print STDERR "\n$direction $type $stream filter\n";
+
+    unless (@data) {
+        print STDERR "  No buckets\n";
+        return;
+    }
+
+    my $c = 1;
+    while (my($btype, $data) = splice @data, 0, 2) {
+        print STDERR "    o bucket $c: $btype\n";
+        print STDERR "[$data]\n";
+        $c++;
+    }
+}
+
+
 1;
