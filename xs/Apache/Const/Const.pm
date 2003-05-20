50d74f69c28d6caa67aab97b11141b9b62f7bac1
Stas Bekman <stas@apache.org>
2003-05-20 01:42:05 +0000

    Apache::DIR_MAGIC_TYPE is now implemented internally PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@69989 13f79535-47bb-0310-9956-ffa450edef68

--- xs/Apache/Const/Const.pm
+++ xs/Apache/Const/Const.pm
@@ -8,8 +8,4 @@ our @ISA = qw(ModPerl::Const);
 
 XSLoader::load(__PACKAGE__, $VERSION);
 
-#XXX: we don't support string constants in the lookup functions
-#always define this one for the moment
-sub Apache::DIR_MAGIC_TYPE () { "httpd/unix-directory" }
-
 1;
