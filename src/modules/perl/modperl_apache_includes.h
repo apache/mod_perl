3cc48733f1aa78199a9ab15309fdff26bf586d15
Stas Bekman <stas@apache.org>
2003-08-06 06:46:35 +0000

    don't try to read PERL_HASH_SEED env var, where apr_env_get is not available (apr < 0.9.3) PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@70052 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/perl/modperl_apache_includes.h
+++ src/modules/perl/modperl_apache_includes.h
@@ -20,6 +20,17 @@
 #include "ap_mpm.h"
 
 #include "apr_version.h"
+
+/* 2.0.36 is APR_0_9_0-dev */
+
+#if APR_MAJOR_VERSION == 0 && APR_MINOR_VERSION == 9 && APR_PATCH_VERSION < 3
+/* apr < 0.9.3 */
+#define APR_VERSION_0_9_3_MINUS
+#else
+/* apr >= 0.9.3 */
+#define APR_VERSION_0_9_3_PLUS
+#endif
+
 #ifndef APR_POLLIN
 /*
  * apr_poll.h introduced around 2.0.40
@@ -36,10 +47,14 @@
 #include "apr_network_io.h"
 #include "apr_general.h"
 #include "apr_uuid.h"
-#include "apr_env.h"
 
 #include "util_filter.h"
 
 #include "util_script.h"
+
+
+#ifdef APR_VERSION_0_9_3_PLUS
+#include "apr_env.h"
+#endif
 
 #endif /* MODPERL_APACHE_INCLUDES_H */
