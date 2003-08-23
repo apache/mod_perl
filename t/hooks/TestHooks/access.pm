2eee9ec158e9b5c5e61537832dd6bf5b9202281e
Stas Bekman <stas@apache.org>
2003-08-23 03:35:16 +0000

    add tracing, so it's easier to prove that all phases are run only once PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@70140 13f79535-47bb-0310-9956-ffa450edef68

--- t/hooks/TestHooks/access.pm
+++ t/hooks/TestHooks/access.pm
@@ -1,11 +1,17 @@
 package TestHooks::access;
 
+# demonstrates the phases execution (sometimes users claim that
+# PerlInitHandler is running more than once
+# run with:
+# t/TEST -trace=debug -v hooks/access
+
 use strict;
 use warnings FATAL => 'all';
 
 use APR::Table ();
 use Apache::Access ();
 use Apache::RequestRec ();
+use Apache::TestTrace;
 
 use Apache::Const -compile => qw(OK FORBIDDEN);
 
@@ -16,12 +22,28 @@ sub handler {
 
     my $fake_ip = $r->headers_in->get('X-Forwarded-For') || "";
 
+    debug "access: " . ($fake_ip =~ $allowed_ips ? "OK\n" : "FORBIDDEN\n");
+
     return Apache::FORBIDDEN unless $fake_ip =~ $allowed_ips;
 
     Apache::OK;
 }
 
+sub fixup { debug "fixup\n"; Apache::OK }
+sub init  { debug "init\n";  Apache::OK }
+
 1;
 __DATA__
-PerlResponseHandler Apache::TestHandler::ok1
-SetHandler modperl
+<NoAutoConfig>
+PerlModule TestHooks::access
+<Location /TestHooks__access>
+    PerlAccessHandler   TestHooks::access
+    PerlInitHandler     TestHooks::access::init
+    PerlFixupHandler    TestHooks::access::fixup
+    PerlResponseHandler Apache::TestHandler::ok1
+    SetHandler modperl
+</Location>
+#<Location />
+#    PerlAccessHandler TestHooks::access
+#</Location>
+</NoAutoConfig>
