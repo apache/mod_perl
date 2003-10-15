3d4e3739d840ce6045c741213fae9abf88a13bfc
Geoffrey Young <geoff@apache.org>
2003-10-15 12:38:36 +0000

    add undef test add harmless notes to error_log for string returns
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@70300 13f79535-47bb-0310-9956-ffa450edef68

--- t/response/TestModperl/status.pm
+++ t/response/TestModperl/status.pm
@@ -14,13 +14,16 @@ sub handler {
     my $rc = shift->args;
 
     if ($rc eq 'die' ||
-        $rc eq Apache::DECLINED) {
+        $rc eq Apache::DECLINED ||
+        $rc =~ m/foo/) {
         t_server_log_error_is_expected();
     }
    
     ModPerl::Util::exit if $rc eq 'exit';
 
-    die if $rc eq 'die';
+    die if $rc eq 'die'; 
+
+    return if $rc eq 'undef';
 
     return $rc;
 }
