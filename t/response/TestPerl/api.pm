376b357f70af53fb781e8045bca873260be777cc
Stas Bekman <stas@apache.org>
2003-10-16 18:23:30 +0000

    perl 5.8.1 w/ ithreads has a bug where it reports the wrong parent pid (as the process was never forked), provide a local workaround (+ new test). PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@70301 13f79535-47bb-0310-9956-ffa450edef68

---
+++ t/response/TestPerl/api.pm
@@ -0,0 +1,33 @@
+package TestPerl::api;
+
+# some perl APIs that we need to test that they work alright under mod_perl
+
+use strict;
+use warnings FATAL => 'all';
+
+use Apache::Test;
+use Apache::TestUtil;
+use Apache::TestTrace;
+
+use Apache::Const -compile => 'OK';
+
+sub handler {
+    my $r = shift;
+
+    plan $r, tests => 1;
+
+    {
+        # 5.8.1 w/ ithreads has a bug where it caches ppid in PL_ppid,
+        # but updates the record only if perl's fork is called, which
+        # is not the case with mod_perl. This results in getppid()
+        # returning 1. A local workaround in the mod_perl source at
+        # the child_init phase fixes the problem.
+        my $ppid = getppid();
+        t_debug "ppid $ppid (pid $$)";
+        ok $ppid > 1;
+    }
+
+    Apache::OK;
+}
+
+1;
