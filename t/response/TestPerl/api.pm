6dd0e252989badbd85057abd86b1ebf5cb5c7fff
Stas Bekman <stas@apache.org>
2003-10-18 19:27:24 +0000

    verify that pid != ppid under threads PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@70305 13f79535-47bb-0310-9956-ffa450edef68

--- t/response/TestPerl/api.pm
+++ t/response/TestPerl/api.pm
@@ -14,7 +14,7 @@ use Apache::Const -compile => 'OK';
 sub handler {
     my $r = shift;
 
-    plan $r, tests => 1;
+    plan $r, tests => 2;
 
     {
         # 5.8.1 w/ ithreads has a bug where it caches ppid in PL_ppid,
@@ -23,8 +23,12 @@ sub handler {
         # returning 1. A local workaround in the mod_perl source at
         # the child_init phase fixes the problem.
         my $ppid = getppid();
-        t_debug "ppid $ppid (pid $$)";
+        t_debug "ppid $ppid";
         ok $ppid > 1;
+
+        # verify that $pid != $ppid
+        t_debug "pid $$";
+        ok $ppid != $$;
     }
 
     Apache::OK;
