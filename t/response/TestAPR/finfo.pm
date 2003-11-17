431f23ad9996f80f581a88af35fc949da9ddd9e6
Geoffrey Young <geoff@apache.org>
2003-11-17 23:31:02 +0000

    add $r->finfo tests
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@70395 13f79535-47bb-0310-9956-ffa450edef68

---
+++ t/response/TestAPR/finfo.pm
@@ -0,0 +1,98 @@
+package TestAPR::finfo;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Apache::Test;
+use Apache::TestUtil;
+use Apache::TestTrace;
+
+use Apache::RequestRec ();
+use APR::Finfo ();
+use APR::Const -compile => qw(SUCCESS FINFO_NORM REG
+                              WREAD WWRITE WEXECUTE);
+
+use File::Spec::Functions qw(catfile);
+use Fcntl qw(:mode);
+
+use Apache::Const -compile => 'OK';
+
+sub handler {
+    my $r = shift;
+
+    plan $r, tests => 17;
+
+    {
+        my $finfo = $r->finfo;
+        my $isa = $finfo->isa('APR::Finfo');
+
+        t_debug "\$r->finfo $finfo";
+        ok $isa;
+    }
+
+    {
+        my $pool = $r->finfo->pool;
+        my $isa = $pool->isa('APR::Pool');
+
+        t_debug "\$r->finfo->pool $pool";
+        ok $isa;
+    }
+
+    my $file = Apache->server_root_relative(catfile qw(htdocs index.html));
+
+    # stat tests
+    {
+        # populate the finfo struct first
+        my $status = APR::Finfo::stat($r->finfo, $file, 
+                                      APR::FINFO_NORM, $r->pool);
+
+        ok t_cmp(APR::SUCCESS,
+                 $status,
+                 "stat $file");
+
+        # now, get information from perl's stat()
+        our ($device, $inode, $protection, $nlink, $user, $group,
+             undef, $size, $atime, $mtime, $ctime) = stat $file;
+
+        # compare stat fields between perl and apr_stat
+        {
+            no strict qw(refs);
+            foreach my $method (qw(device inode nlink user group
+                                   size atime mtime ctime)) {
+
+                ok t_cmp(${$method},
+                         $r->finfo->$method(),
+                         "\$r->finfo->$method()");
+            }
+        }
+
+        # match world bits
+
+        ok t_cmp($protection & S_IROTH,
+                 $r->finfo->protection & APR::WREAD,
+                 '$r->finfo->protection() & APR::WREAD');
+
+        ok t_cmp($protection & S_IWOTH,
+                 $r->finfo->protection & APR::WWRITE,
+                 '$r->finfo->protection() & APR::WWRITE');
+
+        ok t_cmp($protection & S_IXOTH,
+                 $r->finfo->protection & APR::WEXECUTE,
+                 '$r->finfo->protection() & APR::WEXECUTE');
+    }
+
+    # tests for stuff not in perl's stat
+    {
+        ok t_cmp($file,
+                 $r->finfo->fname,
+                 '$r->finfo->fname()');
+
+        ok t_cmp(APR::REG,
+                 $r->finfo->filetype,
+                 '$r->finfo->filetype()');
+    }        
+
+    Apache::OK;
+}
+
+1;
