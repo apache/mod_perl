4c8ee43a8e501f9ef460f91da4c5557226d3c1b6
Stas Bekman <stas@apache.org>
2003-12-19 19:29:23 +0000

    make sure that the local .pl file is loaded (it's not longer removed from %INC by registrycooker, only perlrun does that on behalf of the users)
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@70491 13f79535-47bb-0310-9956-ffa450edef68

--- ModPerl-Registry/t/cgi-bin/require.pl
+++ ModPerl-Registry/t/cgi-bin/require.pl
@@ -9,6 +9,7 @@ my $require = catfile $vars->{serverroot}, 'cgi-bin', 'local-conf.pl';
 print "Content-type: text/plain\n\n";
 
 # XXX: meanwhile we don't chdir to the script's dir
+delete $INC{$require};
 require $require;
 
 print defined $test_require && $test_require;
