eec1ac14e88780423b31d1c6f456a3fd09a51390
Stas Bekman <stas@apache.org>
2003-12-19 07:06:31 +0000

    rewrite the error checking in the connection protocol handlers not to warn, when $bb comes empty, since it's the end of input PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@70490 13f79535-47bb-0310-9956-ffa450edef68

--- t/filter/TestFilter/both_str_con_add.pm
+++ t/filter/TestFilter/both_str_con_add.pm
@@ -54,22 +54,21 @@ sub handler {
     my $bb = APR::Brigade->new($c->pool, $c->bucket_alloc);
 
     for (;;) {
-        my $rv = $c->input_filters->get_brigade($bb,
-                                                Apache::MODE_GETLINE);
-
-        if ($rv != APR::SUCCESS or $bb->empty) {
+        my $rv = $c->input_filters->get_brigade($bb, Apache::MODE_GETLINE);
+        if ($rv != APR::SUCCESS && $rv != APR::EOF) {
             my $error = APR::strerror($rv);
-            unless ($rv == APR::EOF) {
-                warn "[echo_filter] get_brigade: $error\n";
-            }
-            $bb->destroy;
+            warn __PACKAGE__ . ": get_brigade: $error\n";
             last;
         }
+
+        last if $bb->empty;
 
         my $b = APR::Bucket::flush_create($c->bucket_alloc);
         $bb->insert_tail($b);
         $c->output_filters->pass_brigade($bb);
     }
+
+    $bb->destroy;
 
     Apache::OK;
 }
