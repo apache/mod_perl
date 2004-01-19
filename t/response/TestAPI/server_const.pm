b3942cc83978895049e27cb5410f50d59eea6cbe
Geoffrey Young <geoff@apache.org>
2004-01-19 18:35:26 +0000

    add server_const.pm to test Apache::Server constant subroutines
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@70535 13f79535-47bb-0310-9956-ffa450edef68

---
+++ t/response/TestAPI/server_const.pm
@@ -0,0 +1,49 @@
+package TestAPI::server_const;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Apache::Test;
+use Apache::TestUtil;
+use File::Spec::Functions qw(canonpath);
+
+use Apache::ServerUtil ();
+use Apache::Process ();
+
+use APR::Pool ();
+
+use Apache::Const -compile => 'OK';
+
+my $cfg = Apache::Test::config;
+
+my $root  = $cfg->{vars}->{serverroot};
+my $built = $cfg->{httpd_info}->{BUILT};
+my $version = $cfg->{httpd_info}->{VERSION};
+
+sub handler {
+
+    my $r = shift;
+
+    plan $r, tests => 3;
+
+    # test Apache::Server constant subroutines
+
+    ok t_cmp(canonpath($root),
+             canonpath(Apache::server_root),
+             'Apache::Server::server_root()');
+     
+
+    ok t_cmp($built,
+             Apache::Server::get_server_built,
+             'Apache::Server::get_server_built()');
+
+    ok t_cmp($version,
+             Apache::Server::get_server_version,
+             'Apache::Server::get_server_version()');
+
+    Apache::OK;
+}
+
+1;
+
+__END__
