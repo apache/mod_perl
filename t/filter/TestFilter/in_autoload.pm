746f9ce88d665c3c5645957a67df6de968ff3d60
Stas Bekman <stas@apache.org>
2004-01-20 01:26:34 +0000

    test that PerlInputFilterHandler autoloads the module containing the handler (since it's ::handler and not a custom sub name we don't have to explicitly call PerlModule)
    add a new helper response handler
    ModPerl::Test::pass_through_response_handler preloaded at the server
    startup.
    
    no point testing PerlOutputFilterHandler as it does the same
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@70543 13f79535-47bb-0310-9956-ffa450edef68

---
+++ t/filter/TestFilter/in_autoload.pm
@@ -0,0 +1,39 @@
+package TestFilter::in_autoload;
+
+# test that PerlInputFilterHandler autoloads the module containing the
+# handler (since it's ::handler and not a custom sub name we don't
+# have to explicitly call PerlModule)
+#
+# no point testing PerlOutputFilterHandler as it does the same
+
+use strict;
+use warnings FATAL => 'all';
+
+use Apache::Filter ();
+
+use Apache::TestTrace;
+
+use Apache::Const -compile => qw(OK);
+
+sub handler {
+    my $filter = shift;
+
+    while ($filter->read(my $buffer, 1024)) {
+        debug "filter read: $buffer";
+        $filter->print(lc $buffer);
+    }
+
+    return Apache::OK;
+}
+
+1;
+
+__DATA__
+<NoAutoConfig>
+  <Location /TestFilter__in_autoload>
+      SetHandler modperl
+      PerlResponseHandler    ModPerl::Test::pass_through_response_handler
+      # no PerlModule TestFilter::in_load on purpose
+      PerlInputFilterHandler TestFilter::in_autoload
+  </Location>
+</NoAutoConfig>
