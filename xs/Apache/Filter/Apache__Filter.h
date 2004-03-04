588ad1c25137663a0470d7653433d01569bf4bcf
Philippe Chiasson <gozer@apache.org>
2004-03-04 06:01:14 +0000

    Updated to the new Apache License Version 2.0 PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@70672 13f79535-47bb-0310-9956-ffa450edef68

--- xs/Apache/Filter/Apache__Filter.h
+++ xs/Apache/Filter/Apache__Filter.h
@@ -1,3 +1,18 @@
+/* Copyright 2001-2004 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #define mp_xs_sv2_modperl_filter(sv) \
 ((SvROK(sv) && (SvTYPE(SvRV(sv)) == SVt_PVMG)) \
 || (Perl_croak(aTHX_ "argument is not a blessed reference"),0) ? \
