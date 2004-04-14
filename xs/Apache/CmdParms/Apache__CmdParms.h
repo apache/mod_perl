ac9745b637c4f27d00ae4b70cf0892c7bcb7974e
Stas Bekman <stas@apache.org>
2004-04-14 03:41:39 +0000

    tweaks to make the source scanner happy PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@70734 13f79535-47bb-0310-9956-ffa450edef68

--- xs/Apache/CmdParms/Apache__CmdParms.h
+++ xs/Apache/CmdParms/Apache__CmdParms.h
@@ -15,9 +15,10 @@
 
 #include "modperl_module.h"
 
-static MP_INLINE SV *mpxs_Apache__CmdParms_info(pTHX_ cmd_parms *cmd_parms)
+static MP_INLINE
+SV *mpxs_Apache__CmdParms_info(pTHX_ cmd_parms *parms)
 {
-    const char *data = ((modperl_module_cmd_data_t *)cmd_parms->info)->cmd_data;
+    const char *data = ((modperl_module_cmd_data_t *)parms->info)->cmd_data;
 
     if (data) {
         return newSVpv(data, 0);
