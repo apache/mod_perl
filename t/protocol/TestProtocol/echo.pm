3aa5bb1c336ddc36ccaa5f319b5de1aa7fb0ac7f
Stas Bekman <stas@apache.org>
2004-04-22 23:21:36 +0000

    cleanups PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@70749 13f79535-47bb-0310-9956-ffa450edef68

--- t/protocol/TestProtocol/echo.pm
+++ t/protocol/TestProtocol/echo.pm
@@ -20,11 +20,11 @@ sub handler {
     # non-blocking socket
     my $nonblocking = $socket->opt_get(APR::SO_NONBLOCK);
     if ($nonblocking) {
-        my $success = $socket->opt_set(APR::SO_NONBLOCK => 0);
+        $socket->opt_set(APR::SO_NONBLOCK => 0);
 
         # test that we really are in the non-blocking mode
-        $nonblocking = $socket->opt_get(APR::SO_NONBLOCK);
-        die "failed to set non-blocking mode" if $nonblocking;
+        $socket->opt_get(APR::SO_NONBLOCK)
+            or die "failed to set non-blocking mode";
     }
 
     my $buff;
