d20573f9f83af915352c13d0b0d1ae540b56278a
Stas Bekman <stas@apache.org>
2004-04-27 01:26:59 +0000

    revert the previous change and use s/apr_ssize_t/apr_size_t/ to use the right types in the functions PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@70756 13f79535-47bb-0310-9956-ffa450edef68

--- xs/modperl_xs_util.h
+++ xs/modperl_xs_util.h
@@ -97,11 +97,11 @@ arg = *MARK++
 /* XXX: we probably shouldn't croak here */
 #define mpxs_write_loop(func, obj) \
     while (MARK <= SP) { \
-        STRLEN wlen; \
+        apr_size_t wlen; \
         apr_status_t rv; \
         char *buf = SvPV(*MARK, wlen);        \
         MP_TRACE_o(MP_FUNC, "%d bytes [%s]", wlen, buf); \
-        rv = func(aTHX_ obj, buf, &(apr_size_t)wlen);    \
+        rv = func(aTHX_ obj, buf, &wlen);    \
         if (rv != APR_SUCCESS) { \
             Perl_croak(aTHX_ modperl_apr_strerror(rv)); \
         } \
