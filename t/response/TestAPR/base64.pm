604112a5b1452c6307decd6b5d9fba48509bf5b5
Stas Bekman <stas@apache.org>
2004-05-21 23:52:07 +0000

    complete APR::Base64 API test PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@70862 13f79535-47bb-0310-9956-ffa450edef68

--- t/response/TestAPR/base64.pm
+++ t/response/TestAPR/base64.pm
@@ -4,6 +4,7 @@ use strict;
 use warnings FATAL => 'all';
 
 use Apache::Test;
+use Apache::TestUtil;
 
 use APR::Base64 ();
 
@@ -12,15 +13,19 @@ use Apache::Const -compile => 'OK';
 sub handler {
     my $r = shift;
 
-    plan $r, tests => 2;
+    plan $r, tests => 3;
 
-    my $encoded = APR::Base64::encode("$r");
+    my $str = "$r";
+    my $encoded = APR::Base64::encode($str);
 
+    t_debug("encoded string: $encoded");
     ok $encoded;
 
-    my $decoded = APR::Base64::decode($encoded);
+    ok t_cmp(length $encoded,
+             APR::Base64::encode_len(length $str),
+             "encoded length");
 
-    ok $decoded eq "$r";
+    ok t_cmp($str, APR::Base64::decode($encoded), "decode");
 
     Apache::OK;
 }
