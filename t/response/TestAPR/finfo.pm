a1f8071a2dcd0c01022e833fb4c6ebeeee5eb533
Stas Bekman <stas@apache.org>
2004-05-24 19:55:14 +0000

    change stat() to return finfo PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@70868 13f79535-47bb-0310-9956-ffa450edef68

--- t/response/TestAPR/finfo.pm
+++ t/response/TestAPR/finfo.pm
@@ -14,13 +14,13 @@ use constant APACHE_2_0_49 => have_apache_version('2.0.49');
 
 use Apache::RequestRec ();
 use APR::Finfo ();
-use APR::Const -compile => qw(SUCCESS FINFO_NORM REG
-                              WREAD WWRITE WEXECUTE);
 
 use File::Spec::Functions qw(catfile);
 use Fcntl qw(:mode);
 
 use Apache::Const -compile => 'OK';
+use APR::Const    -compile => qw(SUCCESS FINFO_NORM REG
+                                 WREAD WWRITE WEXECUTE);
 
 sub handler {
     my $r = shift;
@@ -44,16 +44,13 @@ sub handler {
     }
 
     my $file = $r->server_root_relative(catfile qw(htdocs index.html));
+    # populate the finfo struct first
+    my $finfo = APR::Finfo::stat($file, APR::FINFO_NORM, $r->pool);
 
-    # stat tests
+    ok $finfo->isa('APR::Finfo');
+
+    # stat tests (same as perl's stat)
     {
-        # populate the finfo struct first
-        my $status = $r->finfo->stat($file, APR::FINFO_NORM, $r->pool);
-
-        ok t_cmp(APR::SUCCESS,
-                 $status,
-                 "stat $file");
-
         # now, get information from perl's stat()
         our ($device, $inode, $protection, $nlink, $user, $group,
              undef, $size, $atime, $mtime, $ctime) = stat $file;
@@ -81,8 +78,8 @@ sub handler {
                 }
                 else {
                     ok t_cmp(${$method},
-                             $r->finfo->$method(),
-                             "\$r->finfo->$method()");
+                             $finfo->$method(),
+                             "\$finfo->$method()");
                 }
             }
         }
@@ -90,20 +87,20 @@ sub handler {
         # match world bits
 
         ok t_cmp($protection & S_IROTH,
-                 $r->finfo->protection & APR::WREAD,
-                 '$r->finfo->protection() & APR::WREAD');
+                 $finfo->protection & APR::WREAD,
+                 '$finfo->protection() & APR::WREAD');
 
         ok t_cmp($protection & S_IWOTH,
-                 $r->finfo->protection & APR::WWRITE,
-                 '$r->finfo->protection() & APR::WWRITE');
+                 $finfo->protection & APR::WWRITE,
+                 '$finfo->protection() & APR::WWRITE');
 
         if (WIN32) {
             skip "different file semantics", 0;
         }
         else {
             ok t_cmp($protection & S_IXOTH,
-                     $r->finfo->protection & APR::WEXECUTE,
-                     '$r->finfo->protection() & APR::WEXECUTE');
+                     $finfo->protection & APR::WEXECUTE,
+                     '$finfo->protection() & APR::WEXECUTE');
         }
     }
 
@@ -115,13 +112,13 @@ sub handler {
         }
         else {
             ok t_cmp($file,
-                     $r->finfo->fname,
-                     '$r->finfo->fname()');
+                     $finfo->fname,
+                     '$finfo->fname()');
         }
 
         ok t_cmp(APR::REG,
-                 $r->finfo->filetype,
-                 '$r->finfo->filetype()');
+                 $finfo->filetype,
+                 '$finfo->filetype()');
     }
 
     Apache::OK;
