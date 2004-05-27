671dae44a30fd600697cb770458dcab5a1c9caeb
Stas Bekman <stas@apache.org>
2004-05-27 02:09:28 +0000

    - crypt is not supported on 3 platforms - add tests for md5 and sha1 PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@70878 13f79535-47bb-0310-9956-ffa450edef68

--- t/response/TestAPR/util.pm
+++ t/response/TestAPR/util.pm
@@ -1,5 +1,7 @@
 package TestAPR::util;
 
+# test APR::Util
+
 use strict;
 use warnings FATAL => 'all';
 
@@ -10,21 +12,42 @@ use APR::Util ();
 
 use Apache::Const -compile => 'OK';
 
+use constant CRYPT_WORKS => $^O !~ /^(MSWin32|beos|NetWare)$/;
+
+my $clear = "this is some text";
+# to get the hash values used:
+# htpasswd -nb[sm] user "this is some text"
+my %hashes = (
+    crypt => 'pHM3JfnL6isho',
+    md5   => '$apr1$Kld6H/..$o5OPPPWslI3zB20S54u9s1',
+    sha1  => '{SHA}A5NpTRa4TethLkfOYlK9NfDYbAY=',
+);
+
 sub handler {
     my $r = shift;
 
-    plan $r, tests => 2;
+    plan $r, tests => 4;
+
+    # password_validate
+    {
+        ok ! APR::Util::password_validate("one", "two");
+
+        while (my($mode, $hash) = each %hashes) {
+            t_debug($mode);
+            if ($mode eq 'crypt' && !CRYPT_WORKS) {
+                t_debug("crypt is not supported on $^O");
+                ok 1; # don't make noise
+            }
+            else {
+                ok APR::Util::password_validate($clear, $hash);
+            }
+        }
+    }
 
 #this function seems unstable on certain platforms
 #    my $blen = 10;
 #    my $bytes = APR::generate_random_bytes($blen);
 #    ok length($bytes) == $blen;
-
-    ok ! APR::Util::password_validate("one", "two");
-
-    my $clear = "pass1";
-    my $hash  = "1fWDc9QWYCWrQ";
-    ok APR::Util::password_validate($clear, $hash);
 
     Apache::OK;
 }
