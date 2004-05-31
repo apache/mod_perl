157ac87a58ba78d14c4a25dfb127d652812f3852
Stas Bekman <stas@apache.org>
2004-05-31 05:51:14 +0000

    - Apache::FilterRec:   - remove the next() accessor since it's not used by Apache at the     moment   - name() is made read-only - improve the test PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@70891 13f79535-47bb-0310-9956-ffa450edef68

--- t/filter/TestFilter/out_str_api.pm
+++ t/filter/TestFilter/out_str_api.pm
@@ -1,5 +1,7 @@
 package TestFilter::out_str_api;
 
+# Test Apache::FilterRec and Apache::Filter accessors
+
 use strict;
 use warnings FATAL => 'all';
 
@@ -9,6 +11,7 @@ use Apache::Filter ();
 use Apache::FilterRec ();
 
 use Apache::Test;
+use Apache::TestUtil;
 use Apache::TestRequest;
 
 use Apache::Const -compile => 'OK';
@@ -30,24 +33,36 @@ sub handler {
 
     tie *STDOUT, $filter;
 
-    plan tests => 6;
+    plan tests => 8;
 
-    ok $data eq $response_data;
+    ok t_cmp($response_data, $data, "response data");
 
     ok $filter->isa('Apache::Filter');
 
-    my $frec = $filter->frec;
+    {
+        my $frec = $filter->frec;
 
-    ok $frec->isa('Apache::FilterRec');
+        ok $frec->isa('Apache::FilterRec');
+        ok t_cmp("modperl_request_output", $frec->name, '$frec->name');
 
-    ok $frec->name;
+        my $next = $filter->next;
+        ok t_cmp("modperl_request_output",
+                 $next->frec->name, '$filter->next->frec->name');
+
+        $next = $next->next;
+        # since we can't ensure that the next filter will be the same,
+        # as it's not under control, just check that we get some name
+        my $name = $next->frec->name;
+        t_debug("next->next name: $name");
+        ok $name;
+    }
 
     my $r = $filter->r;
 
     ok $r->isa('Apache::RequestRec');
 
     my $path = '/' . Apache::TestRequest::module2path(__PACKAGE__);
-    ok $r->uri eq $path;
+    ok t_cmp($path, $r->uri, "path");
 
     untie *STDOUT;
 
@@ -55,6 +70,10 @@ sub handler {
     $filter->remove;
 
     Apache::OK;
+}
+
+sub pass_through {
+    return Apache::DECLINED;
 }
 
 sub response {
@@ -68,6 +87,12 @@ sub response {
 
 1;
 __DATA__
-SetHandler modperl
-PerlModule          TestFilter::out_str_api
-PerlResponseHandler TestFilter::out_str_api::response
+<NoAutoConfig>
+PerlModule TestFilter::out_str_api
+<Location /TestFilter__out_str_api>
+    SetHandler modperl
+    PerlResponseHandler TestFilter::out_str_api::response
+    PerlOutputFilterHandler TestFilter::out_str_api
+    PerlOutputFilterHandler TestFilter::out_str_api::pass_through
+</Location>
+</NoAutoConfig>
