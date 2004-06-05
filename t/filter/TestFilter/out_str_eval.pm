39b7d839b8c6d457f02f5dce8f90c5f6289a812d
Stas Bekman <stas@apache.org>
2004-06-05 05:05:21 +0000

    a test that reproduces the problem with $@ and filters. needs to be resolved PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@70921 13f79535-47bb-0310-9956-ffa450edef68

---
+++ t/filter/TestFilter/out_str_eval.pm
@@ -0,0 +1,42 @@
+package TestFilter::out_str_eval;
+
+# at some point there was a problem when eval {} in a non-filter
+# handler wasn't functioning when a filter was involved. the $@ value
+# was getting lost when t_cmp was doing print of debug values. and a
+# new invocation of a filter handler resets the value of $@.
+
+use strict;
+use warnings FATAL => 'all';
+
+use Apache::RequestRec ();
+use Apache::RequestIO ();
+use Apache::Filter ();
+
+use Apache::Test;
+use Apache::TestUtil;
+
+use Apache::Const -compile => qw(OK DECLINED);
+
+# dummy pass_through filter was good enough to trigger the problem
+sub handler {
+    return Apache::DECLINED;
+}
+
+sub response {
+    my $r = shift;
+
+    plan $r, tests => 1, todo => [1];
+    # XXX: see if we can fix filter handlers to restore the original
+    # $@ when the callback completes
+    eval { i_do_not_exist_really_i_do_not() };
+    ok t_cmp(qr/Undefined subroutine/, $@, "some croak");
+
+    return Apache::OK;
+}
+
+1;
+__DATA__
+SetHandler modperl
+PerlModule          TestFilter::out_str_eval
+PerlResponseHandler TestFilter::out_str_eval::response
+
