f13fe1bb2cd366873afe1c3abf13848ec07989df
Stas Bekman <stas@apache.org>
2004-07-14 08:42:07 +0000

    temporary commit the implementation that doesn't use $bb->cleanup, which fails on certain setups PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@71031 13f79535-47bb-0310-9956-ffa450edef68

--- t/protocol/TestProtocol/echo_bbs2.pm
+++ t/protocol/TestProtocol/echo_bbs2.pm
@@ -4,6 +4,11 @@ package TestProtocol::echo_bbs2;
 # brigade for input and output, using flatten to slurp all the data in
 # the bucket brigade, and cleanup to get rid of the old buckets
 
+# XXX: ideally $bb->cleanup should be used here and no create/destroy
+# bb every time the loop is entered should be done. But it segfaults
+# on certain setups:
+# http://marc.theaimsgroup.com/?l=apache-modperl-dev&m=108967266419527&w=2
+
 use strict;
 use warnings FATAL => 'all';
 
@@ -23,29 +28,30 @@ sub handler {
     # the socket to a blocking IO mode
     $c->client_socket->opt_set(APR::SO_NONBLOCK, 0);
 
-    my $bb = APR::Brigade->new($c->pool, $c->bucket_alloc);
-
     my $last = 0;
     while (1) {
         my $bb_in  = APR::Brigade->new($c->pool, $c->bucket_alloc);
+        my $bb_out = APR::Brigade->new($c->pool, $c->bucket_alloc);
+
         my $rc = $c->input_filters->get_brigade($bb_in,
                                                 Apache::MODE_GETLINE);
         last if $rc == APR::EOF;
         die APR::Error::strerror($rc) unless $rc == APR::SUCCESS;
 
         next unless $bb_in->flatten(my $data);
-        $bb->cleanup;
         #warn "read: [$data]\n";
         last if $data =~ /^[\r\n]+$/;
 
         # transform data here
         my $bucket = APR::Bucket->new(uc $data);
-        $bb->insert_tail($bucket);
+        $bb_out->insert_tail($bucket);
 
-        $c->output_filters->fflush($bb);
+        $c->output_filters->fflush($bb_out);
+
+        # XXX: add DESTROY and remove explicit calls
+        $bb_in->destroy;
+        $bb_out->destroy;
     }
-
-    $bb->destroy;
 
     Apache::OK;
 }
