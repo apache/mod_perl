02864cc78f047c54b2e113c07498e4797b47d021
Stas Bekman <stas@apache.org>
2004-07-22 06:32:41 +0000

    complete very basic Apache::Response tests PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@71074 13f79535-47bb-0310-9956-ffa450edef68

--- t/response/TestAPI/response.pm
+++ t/response/TestAPI/response.pm
@@ -1,5 +1,10 @@
 package TestAPI::response;
 
+# testing Apache::Response methods
+#
+# XXX: a proper test is needed (at the moment just test that methods
+# can be invoked as documented)
+
 use strict;
 use warnings FATAL => 'all';
 
@@ -7,15 +12,18 @@ use Apache::RequestRec ();
 use Apache::Response ();
 
 use Apache::Test;
+use Apache::TestUtil;
 
 use Apache::Const -compile => 'OK';
 
 sub handler {
     my $r = shift;
 
-    plan $r, tests => 6;
+    plan $r, tests => 7;
 
-    ok $r->make_etag(0);
+    my $etag = $r->make_etag();
+    t_debug $etag;
+    ok $etag;
 
     $r->set_content_length(0);
 
@@ -31,9 +39,11 @@ sub handler {
 
     ok $r->mtime == $mtime;
 
+    ok $r->set_keepalive() || 1;
+
     $r->set_last_modified;
 
-    # custom_response is tested in TestAPI::custom_response
+    # $r->custom_response() is tested in TestAPI::custom_response
 
     ok 1;
 
