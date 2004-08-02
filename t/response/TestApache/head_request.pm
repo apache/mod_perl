6a7dc8768e284f76d7fa7b37c900978d58c86854
Stas Bekman <stas@apache.org>
2004-08-02 00:36:43 +0000

    testing nuances of the HEAD request (e.g. when C-L header makes it through) PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@71101 13f79535-47bb-0310-9956-ffa450edef68

---
+++ t/response/TestApache/head_request.pm
@@ -0,0 +1,36 @@
+package TestApache::head_request;
+
+# see the client for the comments
+
+use strict;
+use warnings FATAL => 'all';
+
+use Apache::RequestRec ();
+use Apache::RequestIO ();
+use Apache::Response ();
+
+use Apache::Const -compile => 'OK';
+
+my $body = "This is a response string";
+
+sub handler {
+    my $r = shift;
+
+    $r->content_type('text/plain');
+
+    my $args = $r->args || '';
+
+    if ($args =~ /set_content_length/) {
+        $r->set_content_length(length $body);
+    }
+
+    if ($args =~ /send_body/) {
+        # really could send just about anything, since Apache discards
+        # the response body on HEAD requests
+        $r->print($body);
+    }
+
+    Apache::OK;
+}
+
+1;
