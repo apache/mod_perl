24e27b6d314f2ca4ad073fd9b2f18434389b9b56
Geoffrey Young <geoff@apache.org>
2004-08-03 16:25:21 +0000

    removed support for httpd 2.0.46.  httpd 2.0.47 is now the minimum supported version
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@71119 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/perl/modperl_apache_compat.h
+++ src/modules/perl/modperl_apache_compat.h
@@ -23,27 +23,16 @@ typedef unsigned long apr_os_thread_t;
 typedef void * apr_thread_mutex_t;
 #endif
 
-/* XXX: these backcompat macros can be deleted when we bump up the
- * minimal supported httpd version to 2.0.47 or higher
+/* back compat adjustements for older Apache versions
  * BACK_COMPAT_MARKER: make back compat issues easy to find :)
  */
 
-/* pre-APR_0_9_5 (APACHE_2_0_47)
- * both 2.0.46 and 2.0.47 shipped with 0.9.4 -
- * we need the one that shipped with 2.0.47,
-   which is major mmn 20020903, minor mmn 4 */
-#if ! AP_MODULE_MAGIC_AT_LEAST(20020903,4)
-
-/* added in APACHE_2_0_47/APR_0_9_4 */
-void apr_table_compress(apr_table_t *t, unsigned flags);
-
-#endif /* pre-APR_0_9_5 (APACHE_2_0_47) */
-
-#define modperl_apr_func_not_implemented(func, httpd_ver, apr_ver) \
-    { \
-        dTHX; \
-        Perl_croak(aTHX_ #func "() requires httpd/" #httpd_ver \
-                               " and apr/" #apr_ver " or higher"); \
-    }
+/* use the following format:
+ *     #if ! AP_MODULE_MAGIC_AT_LEAST(20020903,4)
+ *         [compat code]
+ *     #endif
+ * and don't forget to insert comments explaining exactly
+ * which httpd release allows us to remove the compat code
+ */
 
 #endif /* MODPERL_APACHE_COMPAT_H */
