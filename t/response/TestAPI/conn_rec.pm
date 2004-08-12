6f713aa65a75e903d4fbe2d6f507127d9eb9e3f0
Stas Bekman <stas@apache.org>
2004-08-12 06:40:45 +0000

    1) Apache::Connection changes [Stas, "Fred Moyer" <fred /about/taperfriendlymusic.org>]   readwrite => readonly:     pool, base_server, local_addr, remote_addr, remote_ip, remote_host,     aborted, local_ip, local_host, id, conn_config, sbh, bucket_alloc removed: logname
    2) split conn_rec test in two parts (sync tests with the API changes)
    
    PR:
    Obtained from:
    Submitted by:
    Reviewed by:
    
    
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@71178 13f79535-47bb-0310-9956-ffa450edef68

--- t/response/TestAPI/conn_rec.pm
+++ t/response/TestAPI/conn_rec.pm
@@ -1,5 +1,8 @@
 package TestAPI::conn_rec;
 
+# this test module is only for testing fields in the conn_rec listed
+# in apache_structures.map (but some fields are tested in other tests)
+
 use strict;
 use warnings FATAL => 'all';
 
@@ -13,15 +16,12 @@ use Apache::Connection ();
 use Apache::Const -compile => qw(OK REMOTE_HOST REMOTE_NAME
     REMOTE_NOLOOKUP REMOTE_DOUBLE_REV CONN_CLOSE);
 
-#this test module is only for testing fields in the conn_rec
-#listed in apache_structures.map
-
 sub handler {
     my $r = shift;
 
     my $c = $r->connection;
 
-    plan $r, tests => 23;
+    plan $r, tests => 14;
 
     ok $c;
 
@@ -39,9 +39,7 @@ sub handler {
 
     ok $c->remote_host || 1;
 
-    ok $c->remote_logname || 1;
-
-    ok $c->aborted || 1;
+    ok !$c->aborted;
 
     ok t_cmp($c->keepalive,
              Apache::CONN_CLOSE,
@@ -51,31 +49,12 @@ sub handler {
 
     ok $c->local_host || 1;
 
-    ok $c->id || 1;
-
-    #conn_config
+    ok $c->id;
 
     ok $c->notes;
 
-    ok $r->notes;
-
-    #input_filters
-    #output_filters
-    #remain
-
-    # Connection utils (XXX: move to conn_utils.pm?)
-
-    # $c->get_remote_host
-    ok $c->get_remote_host() || 1;
-
-    for (Apache::REMOTE_HOST, Apache::REMOTE_NAME, 
-        Apache::REMOTE_NOLOOKUP, Apache::REMOTE_DOUBLE_REV) {
-        ok $c->get_remote_host($_) || 1;
-    }
-
-    ok $c->get_remote_host(Apache::REMOTE_HOST, 
-                           $r->per_dir_config) || 1;
-    ok $c->get_remote_host(Apache::REMOTE_HOST, $r->per_dir_config) || 1;
+    # XXX: missing tests
+    # conn_config
 
     Apache::OK;
 }
---
+++ t/response/TestAPI/conn_util.pm
@@ -0,0 +1,40 @@
+package TestAPI::conn_util;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Apache::TestUtil;
+use Apache::Test;
+
+use Apache::RequestRec ();
+use Apache::RequestUtil ();
+use Apache::Connection ();
+
+use Apache::Const -compile => qw(OK REMOTE_HOST REMOTE_NAME
+    REMOTE_NOLOOKUP REMOTE_DOUBLE_REV CONN_CLOSE);
+
+sub handler {
+    my $r = shift;
+
+    my $c = $r->connection;
+
+    plan $r, tests => 7;
+
+    ok $c->get_remote_host() || 1;
+
+    for (Apache::REMOTE_HOST, Apache::REMOTE_NAME,
+        Apache::REMOTE_NOLOOKUP, Apache::REMOTE_DOUBLE_REV) {
+        ok $c->get_remote_host($_) || 1;
+    }
+
+    ok $c->get_remote_host(Apache::REMOTE_HOST,
+                           $r->per_dir_config) || 1;
+    ok $c->get_remote_host(Apache::REMOTE_HOST, $r->per_dir_config) || 1;
+
+    # XXX: missing tests
+    # remain
+
+    Apache::OK;
+}
+
+1;
