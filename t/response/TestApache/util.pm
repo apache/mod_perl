0a7a006ef714aff3657929c23b58edb1571e1d63
Stas Bekman <stas@apache.org>
2004-08-14 00:24:16 +0000

    Apache::Util polish PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@71196 13f79535-47bb-0310-9956-ffa450edef68

--- t/response/TestApache/util.pm
+++ t/response/TestApache/util.pm
@@ -27,8 +27,9 @@ my $parse_time_ok = $locale =~ /^en_/ ? 1 : 0;
 sub handler {
     my $r = shift;
 
-    plan $r, tests => 4;
+    plan $r, tests => 8;
 
+    # ht_time
     {
         my $time = time;
         my $fmt = "%a, %d %b %Y %H:%M:%S %Z";
@@ -52,6 +53,36 @@ sub handler {
                  'Apache::Util::ht_time($pool, $time, $fmt, $gmt)', 0);
     }
 
+    # escape_path
+    {
+        my($uri, $received, $expected);
+
+        $uri = "a 'long' file?.html";
+        ($expected = $uri) =~ s/([\s?;])/sprintf "%%%x", ord $1/ge;
+
+        $received = Apache::Util::escape_path($uri, $r->pool);
+        ok t_cmp $received, $expected,
+            "Apache::Util::escape_path / partial=1 / default";
+
+        $received = Apache::Util::escape_path($uri, $r->pool, 1);
+        ok t_cmp $received, $expected,
+            "Apache::Util::escape_path / partial=1 / explicit";
+
+        $received = Apache::Util::escape_path($uri, $r->pool, 0);
+        ok t_cmp $received, $expected,
+            "Apache::Util::escape_path / partial=0";
+
+        $uri = "a 'long' file?.html:";
+        ($expected = $uri) =~ s/([\s?;])/sprintf "%%%x", ord $1/ge;
+        # XXX: why does it prepend ./ only if it sees : or :/?
+        $expected = "./$expected";
+
+        $received = Apache::Util::escape_path($uri, $r->pool, 0);
+        ok t_cmp $received, $expected,
+            "Apache::Util::escape_path / partial=0 / ./ prefix ";
+
+    }
+
     Apache::OK;
 }
 
@@ -62,10 +93,10 @@ sub time_cmp {
     if ($parse_time_ok && $exact_match) {
         my $ptime = APR::Date::parse_http($fmtdate);
         t_debug "fmtdate: $fmtdate";
-        ok t_cmp($ptime, $time, $comment);
+        ok t_cmp $ptime, $time, $comment;
     }
     else {
-        ok t_cmp($fmtdate_ptn, $fmtdate, $comment);
+        ok t_cmp $fmtdate_ptn, $fmtdate, $comment;
     }
 }
 
