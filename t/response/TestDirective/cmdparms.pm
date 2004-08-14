0d0db0ab5e038449ca3cbd3a1705e5e1134e2f75
Philippe Chiasson <gozer@apache.org>
2004-08-14 04:29:50 +0000

    Whitespace and indentation cleanups PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@71197 13f79535-47bb-0310-9956-ffa450edef68

--- t/response/TestDirective/cmdparms.pm
+++ t/response/TestDirective/cmdparms.pm
@@ -5,6 +5,7 @@ use warnings FATAL => 'all';
 
 use Apache::CmdParms ();
 use Apache::Module ();
+
 use Apache::Test;
 use Apache::TestUtil;
 
@@ -34,41 +35,32 @@ our @APACHE_MODULE_COMMANDS = (
     },
 );
   
-my @methods = qw(
-cmd
-context
-directive
-info
-limited
-override
-path
-pool
-server
-temp_pool
-);
+my @methods = qw(cmd context directive info limited override path
+                 pool server temp_pool);
   
 sub TestCmdParms {
-    my ($self, $parms, $args) = @_;
+    my($self, $parms, $args) = @_;
     my $srv_cfg = $self->get_config($parms->server);
     foreach my $method (@methods) {
         $srv_cfg->{$args}{$method} = $parms->$method();
     }
-    $srv_cfg->{$args}{check_ctx} = $parms->check_cmd_context(Apache::NOT_IN_LOCATION);
+    $srv_cfg->{$args}{check_ctx} = 
+        $parms->check_cmd_context(Apache::NOT_IN_LOCATION);
 }
 
 sub get_config {
-    my ($self, $s) = (shift, shift);
+    my($self, $s) = (shift, shift);
     Apache::Module->get_config($self, $s, @_);
 }
 
 ### response handler ###
 sub handler : method {
-    my ($self, $r) = @_;
+    my($self, $r) = @_;
     my $override; 
     my $srv_cfg = $self->get_config($r->server);
-    
+
     plan $r, tests => 6 + ( 8 * keys(%$srv_cfg) );
-    
+
     foreach my $cfg (values %$srv_cfg) {
         ok t_cmp (ref($cfg->{cmd}), 'Apache::Command', 'cmd');
         ok t_cmp (ref($cfg->{context}), 'Apache::ConfVector', 'context');
@@ -79,30 +71,32 @@ sub handler : method {
         ok t_cmp ($cfg->{limited}, -1, 'limited');
         ok t_cmp ($cfg->{info}, 'cmd_data', 'cmd_data');
     }    
-    
+
     my $vhost = $srv_cfg->{Vhost};
-    
+
     $override = Apache::RSRC_CONF   |
                 Apache::OR_INDEXES  |
                 Apache::OR_FILEINFO |
                 Apache::OR_OPTIONS;
-    
+
     ok t_cmp ($vhost->{override}, $override, 'override');
     ok t_cmp ($vhost->{path}, undef, 'path');
     ok t_cmp ($vhost->{check_ctx}, undef, 'check_cmd_ctx');
-    
+
     my $loc = $srv_cfg->{Location};
-    
+
     $override = Apache::ACCESS_CONF |
                 Apache::OR_INDEXES  |
                 Apache::OR_AUTHCFG  |
                 Apache::OR_FILEINFO |
                 Apache::OR_OPTIONS  |
                 Apache::OR_LIMIT;
-    
+
     ok t_cmp ($loc->{override}, $override, 'override');
     ok t_cmp ($loc->{path}, '/TestDirective__cmdparms', 'path');
-    ok t_cmp ($loc->{check_ctx}, KEY . ' cannot occur within <Location> section', 'check_cmd_ctx');
+    ok t_cmp ($loc->{check_ctx}, KEY . 
+              ' cannot occur within <Location> section', 'check_cmd_ctx');
+
     return Apache::OK;
 }
 
