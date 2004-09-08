bb69e514336d646545807317982fe5f6b98e5384
Stas Bekman <stas@apache.org>
2004-09-08 23:46:39 +0000

    avoid the reload debug noise in the error_log PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@71296 13f79535-47bb-0310-9956-ffa450edef68

--- t/response/TestModules/reload.pm
+++ t/response/TestModules/reload.pm
@@ -9,7 +9,7 @@ sub handler {
     my $r = shift;
 
     eval "use Apache::Reload::Test";
-    
+
     Apache::Reload::Test::run($r);
 
     return Apache::OK;
@@ -20,6 +20,6 @@ __END__
 
 PerlModule Apache::Reload
 PerlInitHandler Apache::TestHandler::same_interp_fixup Apache::Reload
-PerlSetVar ReloadDebug On
+PerlSetVar ReloadDebug Off
 PerlSetVar ReloadConstantRedefineWarnings Off
 PerlSetVar ReloadAll Off
