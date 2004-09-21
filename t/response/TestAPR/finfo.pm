91fcce7944a8f71c83b110c0a4eae2b32fa120c2
Stas Bekman <stas@apache.org>
2004-09-21 03:29:18 +0000

    no more APR::Finfo::pool PR: Obtained from: Submitted by: Reviewed by:
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@71360 13f79535-47bb-0310-9956-ffa450edef68

--- t/response/TestAPR/finfo.pm
+++ t/response/TestAPR/finfo.pm
@@ -19,7 +19,7 @@ use APR::Const    -compile => qw(FINFO_NORM);
 sub handler {
     my $r = shift;
 
-    my $tests = 2 + TestAPRlib::finfo::num_of_tests();
+    my $tests = 1 + TestAPRlib::finfo::num_of_tests();
     plan $r, tests => $tests;
 
     {
@@ -27,14 +27,6 @@ sub handler {
         my $isa = $finfo->isa('APR::Finfo');
 
         t_debug "\$r->finfo $finfo";
-        ok $isa;
-    }
-
-    {
-        my $pool = $r->finfo->pool;
-        my $isa = $pool->isa('APR::Pool');
-
-        t_debug "\$r->finfo->pool $pool";
         ok $isa;
     }
 
