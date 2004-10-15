61410dc7f69e0bc479a184237281e7a5b48a18a3
Stas Bekman <stas@apache.org>
2004-10-15 22:28:34 +0000

    use Apache::NOT_FOUND constant instead of hardcoded 404 status
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@71441 13f79535-47bb-0310-9956-ffa450edef68

--- ModPerl-Registry/t/cgi-bin/runtime_error_n_status_change.pl
+++ ModPerl-Registry/t/cgi-bin/runtime_error_n_status_change.pl
@@ -2,7 +2,10 @@ BEGIN {
     use Apache::TestUtil;
     t_server_log_error_is_expected();
 }
+
+use Apache::Const -compile => qw(NOT_FOUND);
+
 my $r = shift;
-$r->status(404);
+$r->status(Apache::NOT_FOUND);
 $r->print("Content-type: text/plain\n\n");
 $r->print(no_such_func());
--- ModPerl-Registry/t/cgi-bin/status_change.pl
+++ ModPerl-Registry/t/cgi-bin/status_change.pl
@@ -1,3 +1,5 @@
+use Apache::Const -compile => qw(NOT_FOUND);
+
 my $r = shift;
-$r->status(404);
+$r->status(Apache::NOT_FOUND);
 $r->print("Content-type: text/plain\n\n");
