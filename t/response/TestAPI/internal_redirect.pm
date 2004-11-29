895f3dbbbcce83cf43c6b9c0c16bca4057d2a54e
Stas Bekman <stas@apache.org>
2004-11-29 15:14:25 +0000

    embed debugging
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@106912 13f79535-47bb-0310-9956-ffa450edef68

--- t/response/TestAPI/internal_redirect.pm
+++ t/response/TestAPI/internal_redirect.pm
@@ -7,6 +7,8 @@ use Apache::RequestRec ();
 use Apache::RequestIO ();
 use Apache::SubRequest ();
 
+use Apache::TestTrace;
+
 use Apache::Const -compile => 'OK';
 
 sub modperl {
@@ -16,11 +18,13 @@ sub modperl {
     if ($args{main}) {
         # sub-req
         $r->content_type('text/plain');
+        debug "modperl: sub-req: response";
         $r->print("internal redirect: $args{main} => modperl");
     }
     else {
         # main-req
         my $redirect_uri = $args{uri};
+        debug "modperl: main-req => $redirect_uri?main=modperl";
         $r->internal_redirect("$redirect_uri?main=modperl");
     }
 
@@ -34,11 +38,13 @@ sub perl_script {
     if ($args{main}) {
         # sub-req
         $r->content_type('text/plain');
+        debug "perl-script: sub-req: response";
         print "internal redirect: $args{main} => perl-script";
     }
     else {
         # main-req
         my $redirect_uri = $args{uri};
+        debug "perl-script: main-req => $redirect_uri?main=perl-script";
         $r->internal_redirect("$redirect_uri?main=perl-script");
     }
 
