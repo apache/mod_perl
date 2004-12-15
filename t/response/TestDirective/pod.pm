e08efb4029e5f2a881a9466cf17fa36a9f9f18fd
Stas Bekman <stas@apache.org>
2004-12-15 04:01:07 +0000

    extend the =pod in config test, to test that the code in =pod is not seen by Apache (even though it'd have been an error with just a string, but better do a real check, in case Apache somehow have ignored the error)
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@111931 13f79535-47bb-0310-9956-ffa450edef68

--- t/response/TestDirective/pod.pm
+++ t/response/TestDirective/pod.pm
@@ -10,13 +10,14 @@ use Apache::Const -compile => 'OK';
 sub handler {
     my $r = shift;
 
-    plan $r, tests => 3;
+    plan $r, tests => 4;
 
-    ok t_cmp($r->dir_config->get('TestDirective__pod_over_worked'), 'yes');
-    ok t_cmp($r->dir_config->get('TestDirective__pod_cut_worked'), 'yes');
+    ok t_cmp $r->dir_config->get('TestDirective__pod_hidden'),      undef;
+    ok t_cmp $r->dir_config->get('TestDirective__pod_over_worked'), 'yes';
+    ok t_cmp $r->dir_config->get('TestDirective__pod_cut_worked'),  'yes';
 
     #XXX: How to test that __END__ works proprely without cloberring all the other tests?
-    ok t_cmp('__END__', '__END__');
+    ok t_cmp '__END__', '__END__';
 
     Apache::OK;
 }
