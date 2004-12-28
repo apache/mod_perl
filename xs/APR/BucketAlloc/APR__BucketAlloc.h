df3642966540930a0541da1f8c4f2cc64056554c
Stas Bekman <stas@apache.org>
2004-12-28 16:36:30 +0000

    APR::Bucket::alloc_create moved  to APR::BucketAlloc::new APR::Bucket::alloc_destroy moved to APR::BucketAlloc::destroy
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@123522 13f79535-47bb-0310-9956-ffa450edef68

---
+++ xs/APR/BucketAlloc/APR__BucketAlloc.h
@@ -0,0 +1,29 @@
+/* Copyright 2001-2004 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "modperl_bucket.h"
+
+#define mpxs_APR__BucketAlloc_destroy apr_bucket_alloc_destroy
+
+static MP_INLINE
+SV *mpxs_APR__BucketAlloc_new(pTHX_ SV *CLASS, SV *p_sv)
+{
+    apr_pool_t *p          = mp_xs_sv2_APR__Pool(p_sv);
+    apr_bucket_alloc_t *ba = apr_bucket_alloc_create(p);
+    SV *ba_sv = sv_setref_pv(NEWSV(0, 0), "APR::BucketAlloc", (void*)ba);
+    //mpxs_add_pool_magic(ba_sv, p_sv);
+    return ba_sv;
+}
+
