801865e88ba1be4a09792ad9f737c2f484a2ef09
Stas Bekman <stas@apache.org>
2005-04-05 16:34:54 +0000

    make sure that the sockets are blocking
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@160197 13f79535-47bb-0310-9956-ffa450edef68

--- t/protocol/TestProtocol/eliza.pm
+++ t/protocol/TestProtocol/eliza.pm
@@ -18,6 +18,17 @@ sub handler {
     my Apache2::Connection $c = shift;
     my APR::Socket $socket = $c->client_socket;
 
+    # starting from Apache 2.0.49 several platforms require you to set
+    # the socket to a blocking IO mode
+    my $nonblocking = $socket->opt_get(APR::Const::SO_NONBLOCK);
+    if ($nonblocking) {
+        $socket->opt_set(APR::Const::SO_NONBLOCK, 0);
+
+        # test that we really *are* in the blocking mode
+        !$socket->opt_get(APR::Const::SO_NONBLOCK)
+            or die "failed to set blocking mode";
+    }
+
     my $last = 0;
     while ($socket->recv(my $buff, BUFF_LEN)) {
         # \r is sent instead of \n if the client is talking over telnet
