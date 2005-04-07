d5674a966677fbfd6f9533f1224f37010c43a0e9
Stas Bekman <stas@apache.org>
2005-04-07 21:48:06 +0000

    fix a bogus dup of the file's content
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@160476 13f79535-47bb-0310-9956-ffa450edef68

--- t/htdocs/vhost/post_config.pl
+++ t/htdocs/vhost/post_config.pl
@@ -6,11 +6,3 @@ use Apache2::ServerUtil ();
 $TestVhost::config::restart_count = Apache2::ServerUtil::restart_count();
 
 1;
-use warnings;
-use strict;
-
-use Apache2::ServerUtil ();
-
-$TestVhost::config::Restart_Count = Apache2::ServerUtil::restart_count();
-
-1;
