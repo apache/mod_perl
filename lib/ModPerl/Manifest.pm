20c800fe290c35530300ab25d521ebaa2a2b3d2e
Stas Bekman <stas@apache.org>
2005-04-12 16:43:53 +0000

    exclude all threads tests from the distro, too many failures
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@161072 13f79535-47bb-0310-9956-ffa450edef68

--- lib/ModPerl/Manifest.pm
+++ lib/ModPerl/Manifest.pm
@@ -116,6 +116,8 @@ eliza
 t/perl/ithreads.t
 t/perl/ithreads2.t
 t/response/TestPerl/ithreads.pm
+t/response/TestPerl/ithreads_args.pm
+t/response/TestPerl/ithreads_eval.pm
 # incomplete
 t/apr-ext/perlio
 # PAUSE breaks if a dist has more than one META.yml. the top-level
