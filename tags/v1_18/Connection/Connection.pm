edd85f0adf06c44b1b98af49106fa4ad724cdd36
Noel J. Bergman <noel@apache.org>
2005-05-23 02:48:14 +0000

    move modperl 1.x tags to tags/
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/branches/1.x-import@177890 13f79535-47bb-0310-9956-ffa450edef68

--- tags/v1_18/Connection/Connection.pm
+++
@@ -1,9 +0,0 @@
-package Apache::Connection;
-
-use Apache ();
-
-$VERSION = '1.00';
-__PACKAGE__->mod_perl::boot($VERSION);
-
-1;
-__END__
--- tags/v1_18/Server/Server.pm
+++
@@ -1,9 +0,0 @@
-package Apache::Server;
-
-use Apache ();
-
-$VERSION = '1.01';
-__PACKAGE__->mod_perl::boot($VERSION);
-
-1;
-__END__
