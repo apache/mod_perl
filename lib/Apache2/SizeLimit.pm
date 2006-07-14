da13216304365f3886a555853ab6a48f97b49d0f
Philip M. Gollucci <pgollucci@apache.org>
2006-07-14 10:17:49 +0000

    sync with http://svn.apache.org/viewvc?rev=419948&view=rev over in Apache-SizeLimit land.
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@421857 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Apache2/SizeLimit.pm
+++ lib/Apache2/SizeLimit.pm
@@ -138,8 +138,16 @@ sub solaris_2_6_size_check {
     return ($size, 0);
 }
 
-sub bsd_size_check {
-    return (BSD::Resource::getrusage())[ 2, 3 ];
+# rss is in KB but ixrss is in BYTES.
+# This is true on at least FreeBSD, OpenBSD, NetBSD
+# Philip M. Gollucci
+sub _bsd_size_check {
+
+    my @results = BSD::Resource::getrusage();
+    my $max_rss   = $results[2];
+    my $max_ixrss = int ( $results[3] / 1024 );
+
+    return ( $max_rss, $max_ixrss );
 }
 
 sub win32_size_check {
