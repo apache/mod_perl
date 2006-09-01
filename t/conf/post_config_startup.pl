606e09695119c712d01213822b874325143a5f6e
Philip M. Gollucci <pgollucci@apache.org>
2006-09-01 05:05:48 +0000

    s/you/You/g
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@439180 13f79535-47bb-0310-9956-ffa450edef68

--- t/conf/post_config_startup.pl
+++ t/conf/post_config_startup.pl
@@ -70,7 +70,7 @@ sub test_apache_size_limit {
     if ($@) {
         # unsupported platform
         die $@ unless $@ =~ /Apache2::SizeLimit not implemented on/
-            or $@ =~ /you must install BSD::Resource/;
+            or $@ =~ /You must install BSD::Resource/;
     }
 }
 
