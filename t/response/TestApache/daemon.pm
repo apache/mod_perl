30e980e116c83db5633ed1f253def6832281c524
Philip M. Gollucci <pgollucci@apache.org>
2006-11-06 22:34:55 +0000

    revert these 2 that snuck in before.
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@471903 13f79535-47bb-0310-9956-ffa450edef68

--- t/response/TestApache/daemon.pm
+++ t/response/TestApache/daemon.pm
@@ -11,7 +11,7 @@ use Apache::TestConfig ();
 use Apache::TestUtil;
 use Apache::Test;
 
-use constant WIN32 => Apache::TestConfig::WIN32;
+use constant WIN32 => Apache::TestConfig::WIN32 || Apache::TestConfig::CYGWIN;
 
 use Apache2::Const -compile => 'OK';
 
