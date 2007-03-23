ce3cf3ef34ae11936f68bc89de2b40eaed1068f7
Fred Moyer <phred@apache.org>
2007-03-23 04:43:57 +0000

    fix unescaped variable interpolation in regular expression
    Reported  by:  Eric Snyder, Perrin Harkins
    Submitted by:  Fred Moyer, Randal Schwartz
    Reviewed  by:  Philip Gollucci, Randy Kobes
    
    
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@521584 13f79535-47bb-0310-9956-ffa450edef68

--- ModPerl-Registry/lib/ModPerl/RegistryCooker.pm
+++ ModPerl-Registry/lib/ModPerl/RegistryCooker.pm
@@ -337,7 +337,7 @@ sub namespace_from_uri {
     my $self = shift;
 
     my $path_info = $self->{REQ}->path_info;
-    my $script_name = $path_info && $self->{URI} =~ /$path_info$/
+    my $script_name = $path_info && $self->{URI} =~ /\Q$path_info\E$/
         ? substr($self->{URI}, 0, length($self->{URI}) - length($path_info))
         : $self->{URI};
 
