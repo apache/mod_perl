d0ddb7649c9f46ba449510f793fed4ccf3459dda
Philip M. Gollucci <pgollucci@apache.org>
2007-03-26 14:58:19 +0000

    apply
    Fix Can't call method "child_terminate" on an undefined value
    By add_cleanup_handler() pass $r to _exit_if_to_big() via shift
    [David Wheeler <david@kineticode.com>]
    
    from http://svn.apache.org/viewvc?view=rev&revision=428211
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/branches/1.x@522536 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Apache/SizeLimit.pm
+++ lib/Apache/SizeLimit.pm
@@ -92,7 +92,7 @@ sub add_cleanup_handler {
     # PerlCleanupHandler phase. That means that there's no way to use
     # $r->get_handlers() to check the results of calling this method.
     $r->push_handlers( 'PerlCleanupHandler',
-                       sub { $class->_exit_if_too_big() } );
+                       sub { $class->_exit_if_too_big(shift) } );
     $r->pnotes( size_limit_cleanup => 1 );
 }
 
