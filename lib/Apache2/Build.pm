31a9550f023413ff124b7803b9eb85eb38edd195
Philippe Chiasson <gozer@apache.org>
2011-12-23 18:20:27 +0000

    Discover apr-2-config from Apache 2.4 onwards.
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@1222775 13f79535-47bb-0310-9956-ffa450edef68

--- lib/Apache2/Build.pm
+++ lib/Apache2/Build.pm
@@ -1153,7 +1153,18 @@ sub apr_bindir {
 
 sub apr_generation {
     my ($self) = @_;
-    return $self->httpd_version_as_int =~ m/2[1-9]\d+/ ? 1 : 0;
+
+    my $httpd_v = $self->httpd_version_as_int;
+
+    if ($httpd_v =~ m/2[4-9]\d+/) {
+        return 2;
+    }
+    elsif ($httpd_v =~ m/2[1-3]\d+/) {
+        return 1;
+    }
+    else {
+        return;
+    }
 }
 
 # returns an array of apr/apu linking flags (--link-ld --libs) if found
@@ -1213,7 +1224,8 @@ sub apru_config_path {
         $self->{$key} = $self->{$mp_key};
     }
 
-    my $config = $self->apr_generation ? "$what-1-config" : "$what-config";
+    my $apr_generation = $self->apr_generation;
+    my $config = $apr_generation ? "$what-${apr_generation}-config" : "$what-config";
 
     if (!$self->{$key}) {
         my @tries = ();
