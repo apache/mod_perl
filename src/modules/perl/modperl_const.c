290b7dd782785a04170eb0889c43d0b6a5f91c7a
Torsten Förtsch <torsten@apache.org>
2012-02-14 19:29:11 +0000

    Fix 2 SV REFCNT bugs:
    - modperl_perl_core_global_init(), the aliasing GV references the aliased
      CV. Thus, it should increment the REFCNT.
    - new_constsub() in modperl_const.c, same story.
    
    The bug has been there for years. Only starting with perl 5.14 it became
    visible by messages like this in the error_log:
    
      Attempt to free unreferenced scalar: SV 0x7fc218, 
          Perl interpreter: 0x7cfdb0 during global destruction.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@1244184 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/perl/modperl_const.c
+++ src/modules/perl/modperl_const.c
@@ -51,7 +51,7 @@ static void new_constsub(pTHX_ constants_lookup lookup,
             gv_init(alias, caller_stash, name, name_len, TRUE);
         }
 
-        GvCV_set(alias, GvCV(*gvp));
+        GvCV_set(alias, MUTABLE_CV(SvREFCNT_inc(GvCV(*gvp))));
     }
 }
 
