fae270022c402dcace5274c0d507e2895b2eb4c7
Torsten Förtsch <torsten@apache.org>
2012-03-12 13:27:30 +0000

    Do not stringify $@ upon exception propagation.
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@1299669 13f79535-47bb-0310-9956-ffa450edef68

--- xs/Apache2/ServerUtil/Apache2__ServerUtil.h
+++ xs/Apache2/ServerUtil/Apache2__ServerUtil.h
@@ -80,7 +80,7 @@ static apr_status_t mpxs_cleanup_run(void *data)
     }
 
     if (SvTRUE(ERRSV)) {
-        Perl_croak(aTHX_ SvPV_nolen(ERRSV));
+        Perl_croak(aTHX_ Nullch);
     }
 
     /* the return value is ignored by apr_pool_destroy anyway */
