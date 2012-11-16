e6650fa534d8b3f1705b1070b60e449b8866a7af
Joe Orton <jorton@apache.org>
2012-11-16 13:06:50 +0000

    CScan gets rather confused if cpp's line number markers appear in the middle of a function declaration
    Submitted by: sf
    
    
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@1410327 13f79535-47bb-0310-9956-ffa450edef68

--- lib/ModPerl/CScan.pm
+++ lib/ModPerl/CScan.pm
@@ -943,7 +943,7 @@ sub new {
     my ($sym) = gensym;
     my $cmd = WIN32 ?
         "$Cpp->{cppstdin} $Defines $addincludes $Cpp->{cppflags} $filename |" :
-        "echo '\#include \"$filename\"' | $Cpp->{cppstdin} $Defines $addincludes $Cpp->{cppflags} $Cpp->{cppminus} |";
+        "echo '\#include \"$filename\"' | $Cpp->{cppstdin} $Defines $addincludes $Cpp->{cppflags} $Cpp->{cppminus} | grep -v '^#' |";
     #my $cmd = "echo '\#include <$filename>' | $Cpp->{cppstdin} $Defines $addincludes $Cpp->{cppflags} $Cpp->{cppminus} |";
 
     (open($sym, $cmd) or die "Cannot open pipe from `$cmd': $!")
