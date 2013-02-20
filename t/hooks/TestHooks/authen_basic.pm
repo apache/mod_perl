7f6f04257780fe7d4d8d991efb4f7310a1b12fc2
Jan Kaluža <jkaluza@apache.org>
2013-02-20 15:01:00 +0000

    * t/hooks/TestHooks/authen_basic.pm: Do not check for ap_requires, it is   not needed in httpd-2.4, because authz provider checks for the valid-user.
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/branches/httpd24@1448205 13f79535-47bb-0310-9956-ffa450edef68

--- t/hooks/TestHooks/authen_basic.pm
+++ t/hooks/TestHooks/authen_basic.pm
@@ -3,9 +3,13 @@ package TestHooks::authen_basic;
 use strict;
 use warnings FATAL => 'all';
 
+use Apache::Test;
+use Apache::TestUtil;
+
 use Apache2::Access ();
 
 use Apache2::Const -compile => qw(OK HTTP_UNAUTHORIZED SERVER_ERROR);
+use constant APACHE24   => have_min_apache_version('2.4.0');
 
 sub handler {
     my $r = shift;
@@ -16,9 +20,13 @@ sub handler {
 
     my $user = $r->user;
 
-    my $requirement = $r->requires->[0]->{requirement};
-
-    return Apache2::Const::SERVER_ERROR unless $requirement eq 'valid-user';
+    # We don't have to check for valid-user in 2.4.0+. If there is bug
+    # in require valid-user handling, it will result in failed test with
+    # bad username/password.
+    if (!APACHE24) {
+        my $requirement = $r->requires->[0]->{requirement};
+        return Apache2::Const::SERVER_ERROR unless $requirement eq 'valid-user';
+    }
 
     unless ($user eq 'dougm' and $sent_pw eq 'foo') {
         $r->note_basic_auth_failure;
