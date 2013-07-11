29f67022b555ef2dfba9e87a8c648216e0801ca2
Steve Hay <stevehay@apache.org>
2013-07-11 07:23:31 +0000

    Don't use AP_DECLARE when declaring/defining our own versions of functions no longer in httpd
    The AP_DECLARE macro marks symbols as "dllexport" when compiled in httpd or as "dllimport" when compiled in third-party code such as mod_perl, but defining a function marked "dllimport" isn't allowed.
    
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/branches/httpd24@1502135 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/perl/modperl_apache_compat.h
+++ src/modules/perl/modperl_apache_compat.h
@@ -50,7 +50,7 @@ AP_DECLARE(const char *) ap_get_server_banner(void);
 #if AP_MODULE_MAGIC_AT_LEAST(20060905,0)
 
 /* removed in APACHE-2.3.0 */
-AP_DECLARE(const char *) ap_get_server_version(void);
+const char * ap_get_server_version(void);
 
 #endif /* since-APACHE-2.3.0 */
 
