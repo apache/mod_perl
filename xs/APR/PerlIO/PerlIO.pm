a37dbb976e4a93f71739e7d166182a5c855fbf5f
Steve Hay <stevehay@apache.org>
2014-02-06 09:15:41 +0000

    Correct perl version check in APR::PerlIO::PERLIO_LAYERS_ARE_ENABLED(). Sadly this was not the cause of t/apache/subprocess.t failing on Windows with httpd-2.4.x.
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/branches/httpd24threading@1565129 13f79535-47bb-0310-9956-ffa450edef68

--- xs/APR/PerlIO/PerlIO.pm
+++ xs/APR/PerlIO/PerlIO.pm
@@ -22,7 +22,7 @@ our $VERSION = '0.009000';
 
 # The PerlIO layer is available only since 5.8.0 (5.7.2@13534)
 use Config;
-use constant PERLIO_LAYERS_ARE_ENABLED => $Config{useperlio} && $] >= 5.00703;
+use constant PERLIO_LAYERS_ARE_ENABLED => $Config{useperlio} && $] >= 5.007003;
 
 use APR ();
 use APR::XSLoader ();
