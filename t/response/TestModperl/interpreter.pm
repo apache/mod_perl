3b323e9732ce4f52bb0945a6e2d67363cd52e73b
Steve Hay <stevehay@apache.org>
2014-06-12 07:46:31 +0000

    Merged revision(s) 1537493 from perl/modperl/branches/httpd24threading: Merged revision(s) 594347 from perl/modperl/branches/threading: Expose modperl_interp_pool_t via ModPerl::InterpPool, modperl_tipool_t via ModPerl::TiPool and modperl_tipool_config_t via ModPerl::TiPoolConfig
    Reviewed-By: Gozer
    Submitted-By: Torsten Foertsch <torsten.foertsch@gmx.net>
    Message-Id: <200710242028.07750.torsten.foertsch@gmx.net>
    
    
    ........
    
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@1602085 13f79535-47bb-0310-9956-ffa450edef68

--- t/response/TestModperl/interpreter.pm
+++ t/response/TestModperl/interpreter.pm
@@ -1,7 +1,5 @@
 package TestModperl::interpreter;
 
-# Modperl::Util tests
-
 use strict;
 use warnings FATAL => 'all';
 
@@ -9,22 +7,68 @@ use Apache::Test;
 use Apache::TestUtil;
 
 use ModPerl::Interpreter ();
+use ModPerl::InterpPool ();
+use ModPerl::TiPool ();
+use ModPerl::TiPoolConfig ();
+use Apache2::MPM ();
 
 use Apache2::Const -compile => 'OK';
 
 sub handler {
     my $r = shift;
 
-    plan $r, tests => 5;
-    
-    my $interp = ModPerl::Interpreter::current();
-    print STDERR Dumper($interp); use Data::Dumper;
-    ok t_cmp ref($interp), 'ModPerl::Interpreter';
-    
-    ok $interp->num_requests > 0;
-    ok $interp->refcnt > 0;
-    ok $interp->mip > 0; 
-    ok $interp->perl > 0; 
+    my $is_threaded=Apache2::MPM->is_threaded;
+
+    plan $r, tests => $is_threaded?17:5;
+
+    my $interp = ModPerl::Interpreter->current;
+
+    ok t_cmp(ref($interp), 'ModPerl::Interpreter',
+	     'interp is a ModPerl::Interpreter');
+
+    ok t_cmp($$interp==${ModPerl::Interpreter::current()}, !!1,
+	     'ModPerl::Interpreter->current == ModPerl::Interpreter::current');
+
+    my $mip = $interp->mip;
+
+    ok t_cmp(ref($mip), 'ModPerl::InterpPool',
+	     'interp->mip is a ModPerl::InterpPool');
+
+    ok t_cmp(${$mip->server}==${$r->server}, !!1,
+	     'mip->server == r->server');
+
+    ok t_cmp(ref($mip->parent), 'ModPerl::Interpreter',
+	     'mip->parent is a ModPerl::Interpreter');
+
+    if($is_threaded) {
+	ok t_cmp($interp->perl!=0, !!1, 'interp->perl');
+	ok t_cmp($interp->num_requests>0, !!1, 'interp->num_requests');
+	ok t_cmp($interp->refcnt>0, !!1, 'interp->refcnt');
+
+	my $tipool = $mip->tipool;
+
+	ok t_cmp(ref($tipool), 'ModPerl::TiPool',
+		 'mip->tipool is a ModPerl::TiPool');
+
+	ok t_cmp($tipool->in_use!=0, !!1, 'tipool->in_use');
+
+	ok t_cmp($tipool->size!=0, !!1, 'tipool->size');
+
+	my $tipcfg = $tipool->cfg;
+
+	ok t_cmp(ref($tipcfg), 'ModPerl::TiPoolConfig',
+		 'tipool->cfg is a ModPerl::TiPoolConfig');
+
+	ok t_cmp($tipcfg->start!=0, !!1, 'tipcfg->start');
+
+	ok t_cmp($tipcfg->min_spare!=0, !!1, 'tipcfg->min_spare');
+
+	ok t_cmp($tipcfg->max_spare!=0, !!1, 'tipcfg->max_spare');
+
+	ok t_cmp($tipcfg->max!=0, !!1, 'tipcfg->max');
+
+	ok t_cmp($tipcfg->max_requests!=0, !!1, 'tipcfg->max_requests');
+    }
 
     Apache2::Const::OK;
 }
