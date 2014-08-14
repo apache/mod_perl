apache/mod_perl

commit 6861f98c21dcd9d388723d5fcadd88359f472804

Stay compatible with httpd-2.2 in Apache2::compat
git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/branches/httpd24@1497279 13f79535-47bb-0310-9956-ffa450edef68

lib/Apache2/compat.pm
@@ -149,7 +149,7 @@ EOI
     require Apache2::Connection;
     require APR::SockAddr;
     require Socket;
-    my $orig_sub = *Apache2::Connection::client_addr{CODE};
+    my $orig_sub = *Apache2::Connection::remote_addr{CODE};
     *Apache2::Connection::remote_addr = sub {
         my $c = shift;
         if (@_) {

commit 5db221ec107f1014973432cd8b1fb6a93050c6b4

This fixes t/compat/conn_rec.t. failures with Apache 2.4, broken with r1497279.
Apache2::compat is supposed to provide mod_perl 1.0 backward
compatibility, so we want to offer remote_addr() there even with
Apache 2.4.

Submitted by: Niko Tyni <ntyni debian.org>


git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@1617891 13f79535-47bb-0310-9956-ffa450edef68

lib/Apache2/compat.pm
@@ -150,7 +150,12 @@ EOI
     require Apache2::Connection;
     require APR::SockAddr;
     require Socket;
-    my $orig_sub = *Apache2::Connection::remote_addr{CODE};
+    my $orig_sub;
+    if (defined *Apache2::Connection::client_addr{CODE}) { # httpd-2.4
+        $orig_sub = *Apache2::Connection::client_addr{CODE};
+    } else { # httpd-2.2
+        $orig_sub = *Apache2::Connection::remote_addr{CODE};
+    }
     *Apache2::Connection::remote_addr = sub {
         my $c = shift;
         if (@_) {