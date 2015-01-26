apache/mod_perl

commit 3d55861c8251ae98e48826390bd74114131fd1dd

Add some missed functions in XS function tables
git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@1638351 13f79535-47bb-0310-9956-ffa450edef68

xs/tables/current24/ModPerl/FunctionTable.pm
@@ -6436,6 +6436,20 @@ $ModPerl::FunctionTable = [
     ]
   },
   {
+    'return_type' => 'void',
+    'name' => 'mpxs_Apache2__Connection_pnotes_kill',
+    'args' => [
+      {
+        'type' => 'PerlInterpreter*',
+        'name' => 'my_perl'
+      },
+      {
+        'type' => 'conn_rec *',
+        'name' => 'c'
+      }
+    ]
+  },
+  {
     'return_type' => 'SV *',
     'name' => 'mpxs_Apache2__Directive_as_hash',
     'attr' => [
@@ -7310,6 +7324,20 @@ $ModPerl::FunctionTable = [
       }
     ]
   },
+  {
+   'return_type' => 'void',
+    'name' => 'mpxs_Apache2__RequestRec_pnotes_kill',
+    'args' => [
+      {
+        'type' => 'PerlInterpreter*',
+        'name' => 'my_perl'
+      },
+      {
+        'type' => 'request_rec *',
+        'name' => 'r'
+      }
+    ]
+  },    
   {
     'return_type' => 'SV *',
     'name' => 'mpxs_Apache2__RequestRec_print',

xs/tables/current/ModPerl/FunctionTable.pm
@@ -8130,6 +8130,24 @@ $ModPerl::FunctionTable = [
   },
   {
     'return_type' => 'SV *',
+    'name' => 'mpxs_apr_thread_rwlock_create',
+    'args' => [
+      {
+        'type' => 'PerlInterpreter *',
+        'name' => 'my_perl'
+      },
+      {
+        'type' => 'SV *',
+        'name' => 'classname'
+      },
+      {
+        'type' => 'SV *',
+        'name' => 'p_sv'
+      }
+    ]
+  },
+  {
+    'return_type' => 'SV *',
     'name' => 'mpxs_apr_uri_parse',
     'args' => [
       {

commit 3bc5dd61cd838cff39fb621ef96ba8db844aef7e

Add another missed function in XS function tables
Fixes Win32 linker error introduced by r1638351.


git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@1654746 13f79535-47bb-0310-9956-ffa450edef68

xs/tables/current24/ModPerl/FunctionTable.pm
@@ -4506,6 +4506,16 @@ $ModPerl::FunctionTable = [
     ]
   },
   {
+    'return_type' => 'void',
+    'name' => 'modperl_pnotes_kill',
+    'args' => [
+      {
+        'type' => 'void *',
+        'name' => 'cl_data'
+      }
+    ]
+  },
+  {
     'return_type' => 'int',
     'name' => 'modperl_post_config_handler',
     'args' => [