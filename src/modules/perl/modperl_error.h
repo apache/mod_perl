37f777882e3ea3f6ab3bc6eac308efabf08ff141
Steve Hay <stevehay@apache.org>
2015-06-04 07:21:28 +0000

    Restore unthreaded build following r1682366
    git-svn-id: https://svn.apache.org/repos/asf/perl/modperl/trunk@1683472 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/perl/modperl_error.h
+++ src/modules/perl/modperl_error.h
@@ -37,6 +37,7 @@ char *modperl_error_strerror(pTHX_ apr_status_t rc);
 
 void modperl_croak(pTHX_ apr_status_t rc, const char* func);
 
+#ifdef USE_ITHREADS
 #define MP_PUTBACK_IF_USED() STMT_START                                 \
     {                                                                   \
         modperl_interp_t *interp = modperl_thx_interp_get(aTHX);        \
@@ -44,6 +45,9 @@ void modperl_croak(pTHX_ apr_status_t rc, const char* func);
             modperl_interp_unselect(interp);                            \
         }                                                               \
     } STMT_END
+#else
+#define MP_PUTBACK_IF_USED() NOOP
+#endif
 
 #define MP_CROAK_PUTBACK(rc, func) STMT_START                           \
     {                                                                   \
